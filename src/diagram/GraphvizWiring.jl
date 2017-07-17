f3739cd370ae722d8bb4ce51810f9c64edcb092f
epatters <ejpatters@gmail.com>
2017-07-16 21:30:34 -0400

    WIP: Draw incoming and outgoing wires in Graphviz wiring diagram.
    The placement of the hidden ports could be better. One idea is to
    replace the single compound node with ports to multiple simple
    nodes. However, the left-to-right order of these nodes must be
    constrained, which requires some hackery:
    
    http://www.graphviz.org/content/order-left-right-boxs-dot-grahviz
    
    Towards #11

--- src/diagram/GraphvizWiring.jl
+++ src/diagram/GraphvizWiring.jl
@@ -31,14 +31,32 @@ function to_graphviz(f::WiringDiagram;
     node_attrs::Graphviz.Attributes=Graphviz.Attributes(),
     edge_attrs::Graphviz.Attributes=Graphviz.Attributes())::Graphviz.Graph
   
-  stmts = Graphviz.Statement[]
+  # Nodes
+  stmts = Graphviz.Statement[
+    # Invisible nodes for incoming and outgoing wires.
+    Graphviz.Node(node_name(input_id(f)),
+                  label=ports_label(Output, length(inputs(f)))),
+    Graphviz.Node(node_name(output_id(f)),
+                  label=ports_label(Input, length(outputs(f))))
+  ]
   for v in box_ids(f)
-    push!(stmts, to_node(v, box(f,v)))
+    # Visible nodes for boxes.
+    label = node_label(v, box(f, v))
+    push!(stmts, Graphviz.Node(node_name(v), label=label))
   end
+  
+  # Edges
   for wire in wires(f)
-    if !any(in.((wire.source.box, wire.target.box), (input_id(f), output_id(f))))
-      push!(stmts, to_edge(wire))
-    end
+    src, tgt = wire.source, wire.target
+    edge = Graphviz.Edge(
+      src = node_name(src.box),
+      src_port = port_name(src.kind, src.port),
+      src_anchor = port_anchor(src.kind),
+      tgt = node_name(tgt.box),
+      tgt_port = port_name(tgt.kind, tgt.port),
+      tgt_anchor = port_anchor(tgt.kind),
+    )
+    push!(stmts, edge)
   end
   
   Graphviz.Digraph(graph_name, stmts;
@@ -53,25 +71,6 @@ function to_graphviz(f::HomExpr; kw...)::Graphviz.Graph
   to_graphviz(to_wiring_diagram(f); kw...)
 end
 
-""" Create a Graphviz node for a box in a wiring diagram.
-"""
-function to_node(v::Int, box::Box)::Graphviz.Node
-  Graphviz.Node(node_name(v), label=node_label(v, box))
-end
-
-""" Create a Graphviz edge for a wire in a wiring diagram.
-"""
-function to_edge(wire::Wire; labels::Bool=false)::Graphviz.Edge
-  # TODO: Support wire labels.
-  src, tgt = wire.source, wire.target
-  Graphviz.Edge(src = node_name(src.box),
-                src_port = port_name(src.kind, src.port),
-                src_anchor = port_anchor(src.kind),
-                tgt = node_name(tgt.box),
-                tgt_port = port_name(tgt.kind, tgt.port),
-                tgt_anchor = port_anchor(tgt.kind))
-end
-
 node_name(v::Int) = "n$v"
 port_name(kind::PortKind, port::Int) = string(kind == Input ? "in" : "out", port)
 port_anchor(kind::PortKind) = kind == Input ? "n" : "s"
@@ -80,16 +79,21 @@ port_anchor(kind::PortKind) = kind == Input ? "n" : "s"
 """
 function node_label(v::Int, box::Box)::Graphviz.Html
   nin, nout = length(inputs(box)), length(outputs(box))
-  top = join("""<TD HEIGHT="0" WIDTH="24" PORT="$(port_name(Input,i))"></TD>"""
-             for i in 1:nin)
-  bot = join("""<TD HEIGHT="0" WIDTH="24" PORT="$(port_name(Output,i))"></TD>"""
-             for i in 1:nout)
   Graphviz.Html("""
     <TABLE BORDER="0" CELLPADDING="0" CELLSPACING="0">
-    <TR><TD><TABLE BORDER="0" CELLPADDING="0" CELLSPACING="0"><TR>$top</TR></TABLE></TD></TR>
+    <TR><TD>$(ports_label(Input,nin).content)</TD></TR>
     <TR><TD BORDER="1" CELLPADDING="4">$(label(box))</TD></TR>
-    <TR><TD><TABLE BORDER="0" CELLPADDING="0" CELLSPACING="0"><TR>$bot</TR></TABLE></TD></TR>
+    <TR><TD>$(ports_label(Output,nout).content)</TD></TR>
     </TABLE>""")
+end
+
+""" Create an "HTML-like" label for the input or output ports of a box.
+"""
+function ports_label(kind::PortKind, nports::Int)::Graphviz.Html
+  rows = join("""<TD HEIGHT="0" WIDTH="24" PORT="$(port_name(kind,i))"></TD>"""
+              for i in 1:nports)
+  Graphviz.Html("""
+    <TABLE BORDER="0" CELLPADDING="0" CELLSPACING="0"><TR>$rows</TR></TABLE>""")
 end
 
 """ Create a label for the main content of a box.
