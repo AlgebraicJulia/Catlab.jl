AlgebraicJulia/Catlab.jl

commit 5fcabf95ef1e0a11aca0b98ded1f357a5e635d3c

BUG: Always parse numbers as values, not references, in JSON S-exprs.
Terms like the following now deserialize properly:

mcopy(A::Ob, n::Int)::Hom(A,opow(A,n))

However, this solution is not ideal because it is no longer possible
to have numeric references. More generally, it is still possible to have
ambiguity between string (or symbolic) values and references. A more robust
solution is to use special objects {"$ref": ...} for references, as in JSON
Schema. On the other hand, that would lead to a more verbose representation.
For now, I'm sticking with the existing encoding.

src/Syntax.jl
@@ -437,25 +437,29 @@ function parse_json_sexpr(syntax_module::Module, sexpr;
     parse_value::Function = default_parse_json_value,
     symbols::Bool = true,
   )
-  typeclass = syntax_module.signature().class()
-  typelens = Dict(cons.name => length(cons.params)
-                  for cons in typeclass.signature.types)
+  signature_module = syntax_module.signature()
+  signature = signature_module.class().signature
+  type_names = Set(cons.name for cons in signature.types)
   
-  function parse_impl(sexpr::Vector, ::Type{Val{true}})
+  # The "value" type is
+  # - false if the sexpr should be parsed as a term (GAT expression)
+  # - true if the sexpr should be parsed as a value (Julia object)
+  function parse_impl(sexpr::Vector, ::Type{Val{false}})
     name = Symbol(sexpr[1])
-    nargs = length(sexpr) - 1
     args = [
-      parse_impl(arg, Val{!(i == 1 && nargs-1 == get(typelens, name, nothing))})
+      parse_impl(arg, Val{
+        (name in type_names && i==1) ||
+        isa(arg, Bool) || isa(arg, Number) || isa(arg, Void)
+      })
       for (i, arg) in enumerate(sexpr[2:end])
     ]
     invoke_term(syntax_module, name, args...)
   end
-  parse_impl(x, ::Type{Val{true}}) = parse_reference(x)
-  parse_impl(x, ::Type{Val{false}}) = parse_term(x)
-  parse_impl(x::String, ::Type{Val{true}}) = parse_reference(symbols ? Symbol(x) : x)
-  parse_impl(x::String, ::Type{Val{false}}) = parse_value(symbols ? Symbol(x) : x)
+  parse_impl(x, ::Type{Val{true}}) = parse_value(x)
+  parse_impl(x::String, ::Type{Val{false}}) = parse_reference(symbols ? Symbol(x) : x)
+  parse_impl(x::String, ::Type{Val{true}}) = parse_value(symbols ? Symbol(x) : x)
   
-  parse_impl(sexpr, Val{true})
+  parse_impl(sexpr, Val{false})
 end
 
 default_parse_json_reference(x) = error("Loading terms by name is not enabled")

commit d3333c6da4628f979ef1c24567256a4af763f01d

BUG: Fix regression in deserialization of JSON S-expressions.
Bug caused by 5fcabf95ef1e0a11aca0b98ded1f357a5e635d3c

src/Syntax.jl
@@ -439,16 +439,17 @@ function parse_json_sexpr(syntax_module::Module, sexpr;
   )
   signature_module = syntax_module.signature()
   signature = signature_module.class().signature
-  type_names = Set(cons.name for cons in signature.types)
+  type_lens = Dict(cons.name => length(cons.params) for cons in signature.types)
   
   # The "value" type is
   # - false if the sexpr should be parsed as a term (GAT expression)
   # - true if the sexpr should be parsed as a value (Julia object)
   function parse_impl(sexpr::Vector, ::Type{Val{false}})
     name = Symbol(sexpr[1])
+    nargs = length(sexpr) - 1
     args = [
       parse_impl(arg, Val{
-        (name in type_names && i==1) ||
+        (i == 1 && get(type_lens, name, nothing) == nargs-1) ||
         isa(arg, Bool) || isa(arg, Number) || isa(arg, Void)
       })
       for (i, arg) in enumerate(sexpr[2:end])