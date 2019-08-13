f519bd67674fe3ac1043b21e9c1aac8066db1936
Evan Patterson <ejpatters@gmail.com>
2019-08-13 17:26:58 -0400

    ENH: Helper function to find series compositions in graph.

--- src/wiring_diagrams/Expressions.jl
+++ src/wiring_diagrams/Expressions.jl
@@ -10,10 +10,12 @@ It is trivial to convert a morphism expression to a wiring diagram, but not to
 go the other way around.
 """
 module WiringDiagramExpressions
-export to_wiring_diagram
+export to_hom_expr, to_wiring_diagram
+
+using LightGraphs
 
 using ...Syntax
-using ...Doctrines: CategoryExpr
+using ...Doctrines: CategoryExpr, ObExpr, HomExpr
 using ..WiringDiagramCore
 
 # Expression -> Diagram
@@ -36,5 +38,34 @@ end
 
 # Diagram -> Expression
 #######################
+
+""" Convert a wiring diagram to a syntactic expression.
+
+The boxes are assumed to carry morphism expressions (`HomExpr`) and the ports to
+carry object expressions (`ObExpr`).
+"""
+function to_hom_expr(diagram::WiringDiagram)::HomExpr
+  # TODO
+end
+
+""" Find series compositions in a graph.
+"""
+function series_in_graph(g::DiGraph)::Set{Vector{Int}}
+  reduced = DiGraph(nv(g))
+  for edge in edges(g)
+    if (length(outneighbors(g,src(edge))) == 1 &&
+        length(inneighbors(g,dst(edge))) == 1)
+      add_edge!(reduced, edge)
+    end
+  end
+  series = Set{Vector{Int}}()
+  for component in weakly_connected_components(reduced)
+    if length(component) > 1
+      sub, vmap = induced_subgraph(reduced, component)
+      push!(series, Int[ vmap[v] for v in topological_sort_by_dfs(sub) ])
+    end
+  end
+  series
+end
 
 end
--- test/wiring_diagrams/Expressions.jl
+++ test/wiring_diagrams/Expressions.jl
@@ -1,7 +1,10 @@
 module TestWiringDiagramExpressions
 
 using Test
+using LightGraphs
+
 using Catlab.Doctrines, Catlab.WiringDiagrams
+import Catlab.WiringDiagrams.WiringDiagramExpressions: series_in_graph
 
 # Expression -> Diagram
 #######################
@@ -18,5 +21,10 @@ fd, gd = WiringDiagram(f), WiringDiagram(g)
 
 # Diagram -> Expression
 #######################
+
+g = union(DiGraph(10), PathDiGraph(3))
+add_edge!(g,5,6); add_edge!(g,8,9); add_edge!(g,9,10)
+
+@test series_in_graph(g) == Set([[1,2,3],[5,6],[8,9,10]])
 
 end
