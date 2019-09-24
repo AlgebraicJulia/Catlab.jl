9e5edf34ab48213fb18406c485dff79e84fe8070
epatters <ejpatters@gmail.com>
2019-09-23 17:59:22 -0700

    DOC: Reference on relation between bubble sort and insertion sort.

--- src/doctrines/Permutations.jl
+++ src/doctrines/Permutations.jl
@@ -45,6 +45,12 @@ end
 An *adjacent transposition*, also known as a *simple transposition*, is a
 transposition of form (i i+1), represented here as simply the number i.
 
+Bubble sort and insertion sort are, in a sense, dual algorithms (Knuth, TAOCP,
+Vol 3: Searching and Sort, Sec 5.3.4: Networks for sorting, Figures 45 & 46). A
+minimal example on which they give different decompositions is the permutation:
+
+  [1,2,3] ↦ [3,2,1]
+
 See also: `decompose_permutation_by_bubble_sort!`
 """
 function decompose_permutation_by_insertion_sort!(σ::Vector{Int})::Vector{Int}
