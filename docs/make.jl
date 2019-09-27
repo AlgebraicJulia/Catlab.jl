7e5a96abdd04e16a42334d9bb26eff8da2cc336a
epatters <ejpatters@gmail.com>
2019-09-26 23:07:50 -0700

    BUILD: Generate Markdown and Jupyter notebooks for literate docs.

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,52 @@
 using Documenter
+using Literate
+
+const literate_dir = Dict(
+  :jl => joinpath(@__DIR__, "literate"),
+  :md => joinpath(@__DIR__, "src", "tutorials"),
+  :nb => joinpath(@__DIR__, "notebooks"),
+)
 
 @info "Loading module"
 using Catlab
 
-@info "Making docs"
+@info "Building tutorial docs"
+for (root, dirs, files) in walkdir(literate_dir[:jl])
+  relroot = relpath(root, literate_dir[:jl])
+  md_dir = joinpath(literate_dir[:md], relroot)
+  nb_dir = joinpath(literate_dir[:nb], relroot)
+  for file in files
+    if last(splitext(file)) == ".jl"
+      Literate.markdown(joinpath(root, file), md_dir;
+        documenter=true, credit=false)
+      Literate.notebook(joinpath(root, file), nb_dir;
+        execute=true, documenter=true, credit=false)
+    end
+  end
+end
+
+@info "Building API docs"
 makedocs(
   modules     = [Catlab],
   format      = Documenter.HTML(),
-  sitename    = "Catlab",
+  sitename    = "Catlab.jl",
   doctest     = false,
   pages       = Any[
     "Catlab.jl" => "index.md",
+    "Tutorials" => Any[
+      "tutorials/wiring_diagrams/wiring_diagram_basics.md",
+      "tutorials/wiring_diagrams/diagrams_and_expressions.md",
+      "tutorials/graphics/graphviz_wiring_diagrams.md",
+      "tutorials/graphics/tikz_wiring_diagrams.md",
+      "tutorials/algebra/algebraic_nets.md",
+    ],
     "APIs"      => Any[
       "apis/index.md",
       "apis/catlab.md",
-      "apis/algebra.md",
       "apis/doctrines.md",
-      "apis/graphics.md",
       "apis/wiringdiagrams.md",
+      "apis/graphics.md",
+      "apis/algebra.md",
     ]
   ]
 )
