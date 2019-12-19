91f78321f819e8c8b24a60bfa4f7a8e03f992ce0
epatters <ejpatters@gmail.com>
2019-12-18 22:11:00 -0800

    ENH: Draw box labels in Compose.jl wiring diagrams.

--- src/graphics/ComposeWiringDiagrams.jl
+++ src/graphics/ComposeWiringDiagrams.jl
@@ -10,8 +10,20 @@ using ...WiringDiagrams
 using ..WiringDiagramLayouts
 using ..WiringDiagramLayouts: lower_corner
 
-# Data types
-############
+# Constants and data types
+##########################
+
+const ComposeProperties = AbstractVector{Compose.Property}
+
+# Default properties for root context, box context, and wire context.
+const default_root_props = Compose.Property[
+  C.font("Serif"),
+  C.stroke("black"),
+]
+const default_box_props = Compose.Property[
+  C.fill("transparent"),
+]
+const default_wire_props = Compose.Property[]
 
 """ A Compose context together with a given width and height.
 
@@ -38,28 +50,39 @@ end
 """ Draw a wiring diagram in Compose.jl.
 """
 function to_composejl(args...;
-    base_unit::Compose.Measure=5*C.mm, kw...)::ComposePicture
+    base_unit::Compose.Measure=5*C.mm,
+    root_props::ComposeProperties=default_root_props,
+    box_props::ComposeProperties=default_box_props,
+    wire_props::ComposeProperties=default_wire_props, kw...)::ComposePicture
   diagram = layout_diagram(args...; kw...)
-  context = to_composejl_context(diagram)
+  context = to_composejl_context(diagram;
+    root_props=root_props, box_props=box_props, wire_props=wire_props)
   size = diagram.value.size
   ComposePicture(context, size[1] * base_unit, size[2] * base_unit)
 end
 
 """ Draw a wiring diagram in Compose.jl using the given layout.
 """
-function to_composejl_context(diagram::WiringDiagram)::Compose.Context
+function to_composejl_context(diagram::WiringDiagram;
+    root_props::ComposeProperties=default_root_props,
+    box_props::ComposeProperties=default_box_props,
+    wire_props::ComposeProperties=default_wire_props)::Compose.Context
   layout = diagram.value::BoxLayout
-  C.compose(
-    C.context(units=C.UnitBox(0, 0, layout.size...)),
+  C.compose(C.context(units=C.UnitBox(0, 0, layout.size...)),
     [C.context();
-      [ C.rectangle(box) for box in boxes(diagram) ];
-      C.fill("transparent"); C.stroke("black");
+      map(to_composejl_context, boxes(diagram));
+      box_props;
     ],
+    root_props...,
   )
 end
 
-Compose.rectangle(box::AbstractBox) = C.rectangle(box.value::BoxLayout)
-Compose.rectangle(layout::BoxLayout) = 
-  C.rectangle(lower_corner(layout)..., layout.size...)
+function to_composejl_context(box::Box)::Compose.Context
+  layout = box.value::BoxLayout
+  C.compose(C.context(),
+    C.rectangle(lower_corner(layout)..., layout.size...),
+    C.text(layout.position..., string(layout.value), C.hcenter, C.vcenter),
+  )
+end
 
 end
