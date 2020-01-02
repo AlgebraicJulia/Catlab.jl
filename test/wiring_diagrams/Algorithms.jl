208f77fcf23e55e988c818409a7ec8629d46a003
epatters <ejpatters@gmail.com>
2020-01-01 21:07:48 -0500

    TST: Don't use expressions in tests of wiring diagram normalization.

--- test/wiring_diagrams/Algorithms.jl
+++ test/wiring_diagrams/Algorithms.jl
@@ -1,75 +1,73 @@
 module TestWiringDiagramAlgorithms
 
 using Test
-using Catlab.Doctrines, Catlab.WiringDiagrams
+using Catlab.WiringDiagrams
+
+A, B, C, D = [ Ports([sym]) for sym in [:A, :B, :C, :D] ]
+I = munit(Ports)
+f = singleton_diagram(Box(:f, [:A], [:B]))
+g = singleton_diagram(Box(:g, [:B], [:C]))
+h = singleton_diagram(Box(:h, [:C], [:D]))
 
 # Normalization
 ###############
 
-A, B, C, D = Ob(FreeCartesianCategory, :A, :B, :C, :D)
-I = munit(FreeCartesianCategory.Ob)
-f = Hom(:f, A, B)
-g = Hom(:g, B, C)
-h = Hom(:h, C, D)
-
 # Normalize copies.
-d = to_wiring_diagram(compose(mcopy(A), otimes(f,f)))
-normalized = to_wiring_diagram(compose(f, mcopy(B)))
+d = compose(mcopy(A), otimes(f,f))
+normalized = compose(f, mcopy(B))
 @test normalize_copy!(d) == normalized
 
-d = to_wiring_diagram(compose(f, mcopy(B), otimes(g,g)))
+d = compose(f, mcopy(B), otimes(g,g))
 normalize_copy!(d)
-normalized = to_wiring_diagram(compose(f, g, mcopy(C)))
+normalized = compose(f, g, mcopy(C))
 perm = sortperm(boxes(d); by=box->box.value)
 @test is_permuted_equal(d, normalized, perm)
 
-d = to_wiring_diagram(compose(mcopy(A), otimes(f,f), otimes(g,g)))
+d = compose(mcopy(A), otimes(f,f), otimes(g,g))
 normalize_copy!(d)
 perm = sortperm(boxes(d); by=box->box.value)
 @test is_permuted_equal(d, normalized, perm)
 
 # Normalize deletions.
-d = to_wiring_diagram(f)
-@test normalize_delete!(d) == to_wiring_diagram(f)
+@test normalize_delete!(copy(f)) == f
 
 d = WiringDiagram(I,I)
-add_box!(d, Box(f))
+add_box!(d, first(boxes(f)))
 @test normalize_delete!(d) == WiringDiagram(I,I)
 
 d = WiringDiagram(A, B)
-fv = add_box!(d, Box(f))
-gv = add_box!(d, Box(g))
-hv = add_box!(d, Box(h))
+fv = add_box!(d, first(boxes(f)))
+gv = add_box!(d, first(boxes(g)))
+hv = add_box!(d, first(boxes(h)))
 add_wires!(d, [
   (input_id(d),1) => (fv,1),
   (fv,1) => (gv,1),
   (gv,1) => (hv,1),
   (fv,1) => (output_id(d),1),
 ])
-@test normalize_delete!(d) == to_wiring_diagram(f)
+@test normalize_delete!(d) == f
 
 # Normalize wiring diagrams representing morphisms in a cartesian category.
-d = to_wiring_diagram(compose(
+d = compose(
   mcopy(A),
   otimes(id(A),mcopy(A)),
   otimes(f,f,f),
   otimes(id(B), id(B), compose(g, delete(C)))
-))
-normalized = to_wiring_diagram(compose(f, mcopy(B)))
+)
+normalized = compose(f, mcopy(B))
 @test normalize_cartesian!(d) == normalized
 
 # Layout
 ########
 
-d = to_wiring_diagram(f)
-@test crossing_minimization_by_sort(d, box_ids(d), sources=[input_id(d)]) == [1]
+@test crossing_minimization_by_sort(f, box_ids(f), sources=[input_id(d)]) == [1]
 
-d = to_wiring_diagram(otimes(f,g))
+d = otimes(f,g)
 @test crossing_minimization_by_sort(d, box_ids(d), sources=[input_id(d)]) == [1,2]
 
 d = WiringDiagram(I,I)
-fv1, fv2 = add_box!(d, Box(f)), add_box!(d, Box(f))
-gv1, gv2 = add_box!(d, Box(g)), add_box!(d, Box(g))
+fv1, fv2 = add_box!(d, first(boxes(f))), add_box!(d, first(boxes(f)))
+gv1, gv2 = add_box!(d, first(boxes(g))), add_box!(d, first(boxes(g)))
 add_wires!(d, [ Wire((fv1,1) => (gv1,1)), Wire((fv2,1),(gv2,1)) ])
 @test crossing_minimization_by_sort(d, [gv1,gv2], sources=[fv1,fv2]) == [1,2]
 @test crossing_minimization_by_sort(d, [fv1,fv2], targets=[gv1,gv2]) == [1,2]
