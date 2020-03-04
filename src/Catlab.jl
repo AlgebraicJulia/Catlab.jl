5bccbef949370ba43298430ea1bb26fafe960418
epatters <ejpatters@gmail.com>
2020-03-04 11:04:57 -0500

    WIP: First cut at doctrines for linear maps and relations.

--- src/Catlab.jl
+++ src/Catlab.jl
@@ -5,5 +5,6 @@ include("doctrines/Doctrines.jl")
 include("wiring_diagrams/WiringDiagrams.jl")
 include("graphics/Graphics.jl")
 include("programs/Programs.jl")
+include("linear_algebra/LinearAlgebra.jl")
 
 end
