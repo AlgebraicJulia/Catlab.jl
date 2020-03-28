c1bd402928a538a2ac80467fe3db7363a2b157ab
epatters <ejpatters@gmail.com>
2020-03-27 20:14:24 -0700

    CLEANUP: Use `Elt`, not `Ob`, for preorders. Fix tests.

--- src/doctrines/Preorders.jl
+++ src/doctrines/Preorders.jl
@@ -1,10 +1,10 @@
 export Preorder, ThinCategory, MonoidalThinCategory, FreePreorder,
-  FreeThinCategory, FreeMononoidalThinCategory,
-  Elt, Leq, ≤, reflexive, transitive
+  FreeThinCategory, FreeMonoidalThinCategory,
+  Elt, Leq, ≤, lhs, rhs, reflexive, transitive
 
-""" Doctrine of *Thin category*
+""" Doctrine of *thin category*
 
-Thin categories have at most one Hom between any two objects.
+Thin categories have at most one morphism between any two objects.
 """
 @theory Category(Ob,Hom) => ThinCategory(Ob,Hom) begin
   f == g ⊣ (A::Ob, B::Ob, f::Hom(A,B), g::Hom(A,B))
@@ -14,22 +14,26 @@ end
   compose(f::Hom, g::Hom) = associate(new(f,g; strict=true))
 end
 
-""" Doctrine of *Preorder*
+""" Doctrine of *preorder*
 
 Preorders encode the axioms of reflexivity and transitivity as term constructors.
 """
-@theory Preorder(Ob,Leq) begin
-  # METATYPES of the GAT
-  Ob::TYPE
-  Leq(lhs::Ob, rhs::Ob)::TYPE
+@theory Preorder(Elt,Leq) begin
+  Elt::TYPE
+  Leq(lhs::Elt, rhs::Elt)::TYPE
   @op (≤) := Leq
-  # Axioms of the mathematical object are lifted to term constructors in the GAT
-  reflexive(A::Ob)::(A≤A) # ∀ A there is a term reflexive(A) which implies A≤A
-  transitive(f::(A≤B), g::(B≤C))::(A≤C) ⊣ (A::Ob, B::Ob, C::Ob)
 
-  # axioms of the GAT are equivalences on terms or simplification rules in the logic
-  f == g ⊣ (A::Ob, B::Ob, f::(A≤B), g::(A≤B))
-  # read as (f⟹ A≤B ∧ g⟹ A≤B) ⟹ f ≡ g
+  # Preorder axioms are lifted to term constructors in the GAT.
+  reflexive(A::Elt)::(A≤A) # ∀ A there is a term reflexive(A) which implies A≤A
+  transitive(f::(A≤B), g::(B≤C))::(A≤C) ⊣ (A::Elt, B::Elt, C::Elt)
+
+  # Axioms of the GAT are equivalences on terms or simplification rules in the logic
+  f == g ⊣ (A::Elt, B::Elt, f::(A≤B), g::(A≤B))
+  # Read as (f⟹ A≤B ∧ g⟹ A≤B) ⟹ f ≡ g
+end
+
+@syntax FreePreorder(ObExpr,HomExpr) Preorder begin
+  transitive(f::Leq, g::Leq) = associate(new(f,g; strict=true))
 end
 
 # a GAT-homomorphism between the Preorder GAT and the ThinCategory GAT
@@ -42,11 +46,6 @@ end
 #   reflexive ↦ id
 #   transitive ↦ compose
 # end
-
-
-@syntax FreePreorder(ObExpr,HomExpr) Preorder begin
-  transitive(f::Leq, g::Leq) = associate(new(f,g; strict=true))
-end
 
 @theory SymmetricMonoidalCategory(Ob,Hom) => MonoidalThinCategory(Ob,Hom) begin
   f == g ⊣ (A::Ob, B::Ob, f::Hom(A,B), g::Hom(A,B))
--- test/doctrines/Preorders.jl
+++ test/doctrines/Preorders.jl
@@ -1,18 +1,14 @@
 using Test
-using Catlab
-using Catlab.Doctrines
 
 x,y,z = Ob(FreeThinCategory, :x, :y, :z)
 f, g = Hom(:f, x, y), Hom(:g, y, z)
 @test dom(compose(f,g)) == x
 @test codom(compose(f,g)) == z
 
-
-x,y,z = Ob(FreePreorder, :x, :y, :z)
-@test_throws UndefVarError f, g = Leq(:f, x, y), Leq(:g, y, z)
-# Preorder doesn't have domain and codomain defined.
-@test_throws UndefVarError dom(transitive(f,g)) == x
-@test_throws UndefVarError codom(transitive(f,g)) == z
+x,y,z = (Elt(FreePreorder.Elt, sym) for sym in (:x, :y, :z))
+f, g = Leq(:f, x, y), Leq(:g, y, z)
+@test lhs(transitive(f,g)) == x
+@test rhs(transitive(f,g)) == z
 
 x,y,z,zz = Ob(FreeMonoidalThinCategory, :x, :y, :z, :zz)
 f, g, h= Hom(:f, x, y), Hom(:g, y, z), Hom(:h, otimes(y,y), z)
@@ -25,28 +21,3 @@ j = Hom(:j, y⊗z, zz)
 @test codom((f⊗id(y))⋅h) == z
 @test dom((f⊗g)⋅j) == x⊗y
 @test codom((f⊗g)⋅j) == zz
-
-
-# # This should work, but doesn't
-# @theory Preorder(Elt,Leq) begin
-#   # METATYPES of the GAT
-#   Elt::TYPE
-#   Leq(lhs::Elt, rhs::Elt)::TYPE
-#   @op (≤) := Leq
-#   # Axioms of the mathematical object are lifted to term constructors in the GAT
-#   reflexive(A::Elt)::(A≤A) # ∀ A there is a term reflexive(A) which implies A≤A
-#   transitive(f::(A≤B), g::(B≤C))::(A≤C) ⊣ (A::Elt, B::Elt, C::Elt)
-#
-#   # axioms of the GAT are equivalences on terms or simplification rules in the logic
-#   f == g ⊣ (A::Ob, B::Ob, f::(A≤B), g::(A≤B))
-#   # read as (f⟹ A≤B ∧ g⟹ A≤B) ⟹ f ≡ g
-# end
-#
-# @syntax FreePreorder(ObExpr,HomExpr) Preorder begin
-#   transitive(f::Leq, g::Leq) = associate(new(f,g; strict=true))
-# end
-#
-# x,y,z = Elt(FreePreorder, [:x, :y, :z])
-# f, g = Leq(:f, x, y), Leq(:g, y, z)
-#
-# @show transitive(f,g)
