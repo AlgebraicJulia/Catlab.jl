3777a6b197300fe7b6613e1ae439a8953b932e1a
epatters <ejpatters@gmail.com>
2020-06-17 14:46:40 -0700

    TST: More tests of pushouts in FinOrd.

--- test/categorical_algebra/FinSets.jl
+++ test/categorical_algebra/FinSets.jl
@@ -18,7 +18,14 @@ h = FinOrdFunction([3,1,2], 3)
 @test compose(id(dom(f)),f) == f
 @test compose(f,id(codom(f))) == f
 
-# Pushout.
+# Pushout from the empty set: the degenerate case of the coproduct.
+f, g = FinOrdFunction([], 2), FinOrdFunction([], 3)
+cospan = pushout(Span(f,g))
+@test base(cospan) == FinOrd(5)
+@test left(cospan) == FinOrdFunction([1,2], 5)
+@test right(cospan) == FinOrdFunction([3,4,5], 5)
+
+# Pushout from a singleton set.
 f, g = FinOrdFunction([1], 2), FinOrdFunction([2], 3)
 cospan = pushout(Span(f,g))
 @test base(cospan) == FinOrd(4)
@@ -26,5 +33,14 @@ h, k = left(cospan), right(cospan)
 @test compose(f,h) == compose(g,k)
 @test h == FinOrdFunction([1,2], 4)
 @test k == FinOrdFunction([3,1,4], 4)
+
+# Pushout from a two-element set, with non-injective legs.
+f, g = FinOrdFunction([1,1], 2), FinOrdFunction([1,2], 2)
+cospan = pushout(Span(f,g))
+@test base(cospan) == FinOrd(2)
+h, k = left(cospan), right(cospan)
+@test compose(f,h) == compose(g,k)
+@test h == FinOrdFunction([1,2], 2)
+@test k == FinOrdFunction([1,1], 2)
 
 end
