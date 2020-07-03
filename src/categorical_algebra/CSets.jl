476989dfedc5de863a3731609a8b18ddc08d060d
Evan Patterson <evan@epatters.org>
2020-07-02 22:48:34 -0700

    WIP: Implement `add_part!` for C-sets using generated function.
    Seems to work but unit tests are needed.

--- src/categorical_algebra/CSets.jl
+++ src/categorical_algebra/CSets.jl
@@ -4,16 +4,25 @@ module CSets
 export CSet, nparts, subpart, incident, add_part!
 
 using AutoHashEquals
-using LabelledArrays
+using LabelledArrays, StaticArrays
 
-@auto_hash_equals struct CSet{
-    NParts <: (SLArray{Tuple{N},Int,1} where N),
-    SubParts <: (SLArray{Tuple{N},Vector{Int},1} where N),
-    Incident <: (SLArray{Tuple{N},Vector{Vector{Int}},1} where N)
-  }
-  nparts::NParts
-  subparts::SubParts
-  incident::Incident
+using ...Syntax, ...Present
+using ...Theories: Category
+
+# C-sets
+########
+
+@auto_hash_equals mutable struct CSet{Ob,Hom,Dom,Codom,NOb,NHom}
+  nparts::SLArray{Tuple{NOb},Int,1,NOb,Ob}
+  subparts::SLArray{Tuple{NHom},Vector{Int},1,NHom,Hom}
+  incident::SLArray{Tuple{NHom},Vector{Vector{Int}},1,NHom,Hom}
+end
+
+function CSet{Ob,Hom,Dom,Codom,NOb,NHom}() where {Ob,Hom,Dom,Codom,NOb,NHom}
+  CSet{Ob,Hom,Dom,Codom,NOb,NHom}(
+    SLArray{Tuple{NOb},Ob}(zeros(SVector{NOb,Int})),
+    SLArray{Tuple{NHom},Hom}(copies(SVector{NHom,Vector{Int}})),
+    SLArray{Tuple{NHom},Hom}(copies(SVector{NHom,Vector{Vector{Int}}})))
 end
 
 nparts(cset::CSet, type) = cset.nparts[type]
@@ -25,13 +34,47 @@ end
 
 incident(cset::CSet, name, part::Int) = cset.incident[name][part]
 
-function add_part!(cset::CSet, type, subparts)
-  part = cset.nparts[type] += 1
-  for (name, subpart) in pairs(subparts)
-    push!(cset.subparts[name], subpart)
-    push!(cset.incident[name][subpart], part)
+add_part!(cset::CSet, type) = add_part!(cset, type, NamedTuple())
+add_part!(cset::CSet, type, subparts) = _add_part!(cset, Val(type), subparts)
+
+@generated function _add_part!(cset::CSet{obs,homs,doms,codoms}, ::Val{type},
+    subparts::NamedTuple{sub}) where {obs,homs,doms,codoms,type,sub}
+  ob = NamedTuple{obs}(eachindex(obs))[type]
+  out_homs = Tuple(homs[i] for (i, dom) in enumerate(doms) if dom == ob)
+  in_homs = Tuple(homs[i] for (i, codom) in enumerate(codoms) if codom == ob)
+  @assert sub ⊆ out_homs "Given subparts $sub not among $out_homs"
+  undef_out_homs = Tuple(hom for hom in out_homs if hom ∉ sub)
+
+  return quote
+    part = cset.nparts.$type + 1
+    cset.nparts = SLVector(cset.nparts; $type=part)
+    for name in $(in_homs)
+      push!(cset.incident[name], Int[])
+    end
+    for name in $(undef_out_homs)
+      push!(cset.subparts[name], 0)
+    end
+    for (name, subpart) in pairs(subparts)
+      push!(cset.subparts[name], subpart)
+      push!(cset.incident[name][subpart], part)
+    end
+    part
   end
-  part
+end
+
+# Static arrays
+###############
+
+# Make static array of copies of object, using nullary constructor.
+# Based on StaticArrays.jl/src/arraymath.jl
+@inline copies(::Type{SA}) where {SA <: StaticArray} = _copies(Size(SA), SA)
+@generated function _copies(::Size{s}, ::Type{SA}) where {s, SA <: StaticArray}
+  T = eltype(SA)
+  v = [:($T()) for i = 1:prod(s)]
+  return quote
+    Base.@_inline_meta
+    $SA(tuple($(v...)))
+  end
 end
 
 end
