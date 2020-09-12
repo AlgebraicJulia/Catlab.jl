60edae059b4b3263aecf2156c6cf07263fdd35bc
Evan Patterson <evan@epatters.org>
2020-09-12 16:36:24 -0700

    TST: Universal property of pullbacks and pushouts in FinSet.

--- test/categorical_algebra/FinSets.jl
+++ test/categorical_algebra/FinSets.jl
@@ -4,8 +4,8 @@ using Test
 using Catlab.Theories, Catlab.CategoricalAlgebra
 using Catlab.CategoricalAlgebra.FinSets
 
-# Category of finite ordinals
-#############################
+# Category of finite sets: skeleton
+###################################
 
 f = FinFunction([1,3,4], 5)
 g = FinFunction([1,1,2,2,3], 3)
@@ -26,7 +26,7 @@ h = FinFunction([3,1,2], 3)
 @test force(compose(f,id(codom(f)))) == f
 
 # Limits
-########
+#-------
 
 # Terminal object.
 @test ob(terminal(FinSet{Int})) == FinSet(1)
@@ -81,11 +81,19 @@ lim = pullback(FinFunction([1,1,3,2],4), FinFunction([1,1,4,2],4))
 @test force(proj1(lim)) == FinFunction([1,2,1,2,4], 4)
 @test force(proj2(lim)) == FinFunction([1,1,2,2,4], 4)
 
+f, g = FinFunction([1,2,4]), FinFunction([2,1,4])
+@test force(pair(lim,f,g) ⋅ proj1(lim)) == f
+@test force(pair(lim,f,g) ⋅ proj2(lim)) == g
+
 # Pullback from a singleton set: the degenerate case of a product.
 lim = pullback(FinFunction([1,1]), FinFunction([1,1,1]))
 @test ob(lim) == FinSet(6)
 @test force(proj1(lim)) == FinFunction([1,2,1,2,1,2])
 @test force(proj2(lim)) == FinFunction([1,1,2,2,3,3])
+
+f, g = FinFunction([1,1,2]), FinFunction([3,2,1])
+@test force(pair(lim,f,g) ⋅ proj1(lim)) == f
+@test force(pair(lim,f,g) ⋅ proj2(lim)) == g
 
 # Pullback using generic limit interface
 f, g = FinFunction([1,1,3,2],4), FinFunction([1,1,4,2],4)
@@ -95,7 +103,7 @@ lim = limit(FreeDiagram([FinSet(4),FinSet(4),FinSet(4)], [(1,3,f),(2,3,g)]))
   [FinFunction([1,2,1,2,4],4), FinFunction([1,1,2,2,4],4)]
 
 # Colimits
-##########
+#---------
 
 # Initial object.
 @test ob(initial(FinSet{Int})) == FinSet(0)
@@ -149,40 +157,50 @@ colim = pushout(f,g)
 @test coproj1(colim) == FinFunction([1,2], 5)
 @test coproj2(colim) == FinFunction([3,4,5], 5)
 
+h, k = FinFunction([3,5], 5), FinFunction([1,2,3], 5)
+@test force(coproj1(colim) ⋅ copair(colim,h,k)) == h
+@test force(coproj2(colim) ⋅ copair(colim,h,k)) == k
+
 # Pushout from a singleton set.
 f, g = FinFunction([1], 2), FinFunction([2], 3)
 colim = pushout(f,g)
 @test ob(colim) == FinSet(4)
-h, k = colim
-@test compose(f,h) == compose(g,k)
-@test h == FinFunction([1,2], 4)
-@test k == FinFunction([3,1,4], 4)
+ι1, ι2 = colim
+@test compose(f,ι1) == compose(g,ι2)
+@test ι1 == FinFunction([1,2], 4)
+@test ι2 == FinFunction([3,1,4], 4)
+
+h, k = FinFunction([3,5]), FinFunction([1,3,5])
+@test force(coproj1(colim) ⋅ copair(colim,h,k)) == h
+@test force(coproj2(colim) ⋅ copair(colim,h,k)) == k
+k = FinFunction([1,2,5])
+@test_throws AssertionError copair(colim,h,k)
 
 # Same thing with generic colimit interface
 diag = FreeDiagram([FinSet(1),FinSet(2),FinSet(3)],[(1,2,f), (1,3,g)])
 colim = colimit(diag)
 @test ob(colim) == FinSet(4)
-_, h, k = colim
-@test compose(f,h) == compose(g,k)
-@test h == FinFunction([1,2], 4)
-@test k == FinFunction([3,1,4], 4)
+_, ι1, ι2 = colim
+@test compose(f,ι1) == compose(g,ι2)
+@test ι1 == FinFunction([1,2], 4)
+@test ι2 == FinFunction([3,1,4], 4)
 
 # Pushout from a two-element set, with non-injective legs.
 f, g = FinFunction([1,1], 2), FinFunction([1,2], 2)
 colim = pushout(f,g)
 @test ob(colim) == FinSet(2)
-h, k = colim
-@test compose(f,h) == compose(g,k)
-@test h == FinFunction([1,2], 2)
-@test k == FinFunction([1,1], 2)
+ι1, ι2 = colim
+@test compose(f,ι1) == compose(g,ι2)
+@test ι1 == FinFunction([1,2], 2)
+@test ι2 == FinFunction([1,1], 2)
 
 # Same thing with generic colimit interface
 diag = FreeDiagram([FinSet(2),FinSet(2),FinSet(2)],[(1,2,f),(1,3,g)])
 colim = colimit(diag)
 @test ob(colim) == FinSet(2)
-_, h, k = colim
-@test compose(f,h) == compose(g,k)
-@test h == FinFunction([1,2], 2)
-@test k == FinFunction([1,1], 2)
+_, ι1, ι2 = colim
+@test compose(f,ι1) == compose(g,ι2)
+@test ι1 == FinFunction([1,2], 2)
+@test ι2 == FinFunction([1,1], 2)
 
 end
