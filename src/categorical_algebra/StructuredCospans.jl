42fd711921977c4c73809e93f81ba9016bc323e6
Evan Patterson <evan@epatters.org>
2020-09-24 17:41:55 -0700

    ENH: Structured cospans of C-sets with sub-C-sets in feet.
    E.g., open graphs whose vertices have data attributes.

--- src/categorical_algebra/StructuredCospans.jl
+++ src/categorical_algebra/StructuredCospans.jl
@@ -7,6 +7,8 @@ module StructuredCospans
 export StructuredMulticospan, StructuredCospan, StructuredCospanOb,
   OpenCSetTypes, OpenACSetTypes
 
+using Compat: only
+
 using AutoHashEquals
 using StaticArrays: StaticVector, SVector
 
@@ -119,68 +121,121 @@ end
 # Structured cospans of C-sets
 ##############################
 
-struct FinSetDiscreteACSet{ob₀, X <: AbstractACSet} end
-struct DiscreteACSet{A <: AbstractACSet, X <: AbstractACSet} end
-
 """ Create types for open C-sets from a C-set type.
 
-Returns two types, for objects and morphisms (structured cospans).
+Returns two types, for objects, a subtype of [`StructuredCospanOb`](@ref), and
+for morphisms, a subtype of [`StructuredCospan`](@ref).
 
 See also: [`OpenACSetTypes`](@ref).
 """
-function OpenCSetTypes(::Type{X}, ob₀::Symbol) where {X<:AbstractCSet}
+function OpenCSetTypes(::Type{X}, ob₀::Symbol) where
+    {CD<:CatDesc, X<:AbstractCSet{CD}}
+  @assert ob₀ ∈ CD.ob
   L = FinSetDiscreteACSet{ob₀, X}
   (StructuredCospanOb{L}, StructuredCospan{L})
 end
 
 """ Create types for open attributed C-sets from an attributed C-set type.
 
-TODO
+The resulting types, for objects and morphisms, each have the same type
+parameters for data types as the original type.
+
+See also: [`OpenCSetTypes`](@ref).
 """
 function OpenACSetTypes(::Type{X}, ob₀::Symbol) where
     {CD<:CatDesc, AD<:AttrDesc{CD}, X<:AbstractACSet{CD,AD}}
   @assert ob₀ ∈ CD.ob
-  type_vars = [ TypeVar(data) for data in AD.data ]
+  type_vars = map(TypeVar, AD.data)
   attrs₀ = [ i for (i,j) in enumerate(AD.adom) if CD.ob[j] == ob₀ ]
   L = if isempty(attrs₀)
     FinSetDiscreteACSet{ob₀, X{type_vars...}}
   else
     CD₀ = CatDesc{(ob₀,),(),(),()}
-    AD₀ = AttrDesc{CD₀,AD.Data,AD.Attr[attrs₀],AD.ADom[attrs₀],AD.ACodom[attrs₀]}
+    AD₀ = AttrDesc{CD₀,AD.data,AD.attr[attrs₀],AD.adom[attrs₀],AD.acodom[attrs₀]}
     DiscreteACSet{ACSet{CD₀,AD₀,Tuple{type_vars...},(),()}, X{type_vars...}}
   end
   (foldr(UnionAll, type_vars, init=StructuredCospanOb{L}),
    foldr(UnionAll, type_vars, init=StructuredCospan{L}))
 end
 
-function (::Type{L})(a::FinSet{Int}) where
-    {ob₀, X, L <: FinSetDiscreteACSet{ob₀,X}}
-  x = X()
-  add_parts!(x, ob₀, length(a))
-  return x
-end
-function (::Type{L})(a::AbstractACSet) where
-    {CD₀, AD₀, A <: AbstractACSet{CD₀,AD₀}, X, L <: DiscreteACSet{A,X}}
-  x = X()
-  for ob in CD₀.ob
-    add_parts!(x, ob, nparts(a, ob))
-  end
-  for attr in AD₀.attr
-    set_subpart!(x, attr, subpart(a, attr))
-  end
-  return x
+abstract type AbstractDiscreteACSet{X <: AbstractACSet} end
+
+StructuredCospan{L}(x::AbstractACSet, f::FinFunction{Int},
+                    g::FinFunction{Int}) where {L<:AbstractDiscreteACSet} =
+ StructuredCospan{L}(x, Cospan(f, g))
+
+""" A functor L: FinSet → C-Set giving the discrete C-set wrt an object in C.
+
+This functor has a right adjoint R: C-Set → FinSet giving the underlying set at
+that object. Instead of instantiating this type directly, you should use
+[`OpenCSetTypes`](@ref) or [`OpenACSetTypes`](@ref).
+"""
+struct FinSetDiscreteACSet{ob₀, X} <: AbstractDiscreteACSet{X} end
+
+""" A functor L: C₀-Set → C-Set giving the discrete C-set for C₀.
+
+Here C₀ is assumed to contain a single object from C and the discreteness is
+with respect to this object. The functor L has a right adjoint R: C-Set → C₀-Set
+forgetting the rest of C. Data attributes of the chosen object are preserved.
+"""
+struct DiscreteACSet{A <: AbstractACSet, X} <: AbstractDiscreteACSet{X} end
+
+function StructuredCospan{L}(
+    x::AbstractACSet, cospan::Cospan{<:FinSet{Int}}) where
+    {A, L<:DiscreteACSet{A}}
+  a = A()
+  copy_common_parts!(a, x)
+  f, g = cospan
+  ϕ, ψ = induced_transformation(a, f), induced_transformation(a, g)
+  StructuredCospan{L}(x, Cospan(a, ϕ, ψ))
 end
 
+""" C-set transformation b → a induced by function `f` into parts of `a`.
+"""
+function induced_transformation(a::A, f::FinFunction{Int}) where
+    {CD, AD, A <: AbstractACSet{CD,AD}}
+  ob = only(CD.ob)
+  @assert nparts(a, ob) == length(codom(f))
+  b = A()
+  add_parts!(b, ob, length(dom(f)))
+  f_vec = collect(f)
+  for attr in AD.attr
+    set_subpart!(b, attr, subpart(a, f_vec, attr))
+  end
+  ACSetTransformation((; ob => f), b, a)
+end
+
+""" Apply left adjoint L: FinSet → C-Set to object.
+"""
+function (::Type{L})(a::FinSet{Int}) where {ob₀,X,L<:FinSetDiscreteACSet{ob₀,X}}
+  x = X()
+  add_parts!(x, ob₀, length(a))
+  x
+end
+
+""" Apply left adjoint L: C₀-Set → C-Set to object.
+"""
+function (::Type{L})(a::AbstractACSet) where {A,X,L<:DiscreteACSet{A,X}}
+  x = X()
+  copy_common_parts!(x, a)
+  x
+end
+
+""" Apply left adjoint L: FinSet → C-Set to morphism.
+"""
 function (::Type{L})(f::FinFunction{Int}) where
     {ob₀, L <: FinSetDiscreteACSet{ob₀}}
   ACSetTransformation((; ob₀ => f), L(dom(f)), L(codom(f)))
 end
+
+""" Apply left adjoint L: C₀-Set → C-Set to morphism.
+"""
 function (::Type{L})(ϕ::ACSetTransformation) where {L <: DiscreteACSet}
   ACSetTransformation(components(ϕ), L(dom(ϕ)), L(codom(ϕ)))
 end
 
 """ Convert morphism a → R(x) to morphism L(a) → x using discrete-forgetful
-adjunction L ⊣ R.
+adjunction L ⊣ R: A ↔ X.
 """
 function shift_left(::Type{L}, x::AbstractACSet, f::FinFunction{Int}) where
     {ob₀, L <: FinSetDiscreteACSet{ob₀}}
@@ -189,6 +244,28 @@ end
 function shift_left(::Type{L}, x::AbstractACSet, ϕ::ACSetTransformation) where
     {L <: DiscreteACSet}
   ACSetTransformation(components(ϕ), L(dom(ϕ)), x)
+end
+
+""" Copy parts into a C-set from a C′-set along intersection of C and C′.
+
+TODO: Both more and less general than `copy_parts!` in the `CSets` module: more
+general in allowing C != C′, but less general in not copying subparts, only
+common data attributes. Ideally this functionality would be unified.
+"""
+@generated function copy_common_parts!(to::ACSet{CD,AD}, from::ACSet{CD′,AD′}) where
+    {CD, AD, CD′, AD′}
+  obs = intersect(CD.ob, CD′.ob)
+  attrs = intersect(AD.attr, AD′.attr)
+  @assert all(dom(AD, attr) == dom(AD′, attr) for attr in attrs)
+  Expr(:block,
+    :(newparts = (; $(map(obs) do ob
+        Expr(:kw, ob, :(add_parts!(to, $(QuoteNode(ob)),
+                                   nparts(from, $(QuoteNode(ob))))))
+        end...))),
+    map(attrs) do attr
+      :(set_subpart!(to, newparts.$(dom(AD, attr)), $(QuoteNode(attr)),
+                     subpart(from, $(QuoteNode(attr)))))
+    end...)
 end
 
 end
--- test/categorical_algebra/StructuredCospans.jl
+++ test/categorical_algebra/StructuredCospans.jl
@@ -15,7 +15,7 @@ const OpenGraphOb, OpenGraph = OpenCSetTypes(Graph, :V)
 # Directed fork as open graph with one input and two outputs.
 g0 = Graph(4)
 add_edges!(g0, [1,2,2], [2,3,4])
-g = OpenGraph(g0, Cospan(FinFunction([1], 4), FinFunction([3,4], 4)))
+g = OpenGraph(g0, FinFunction([1],4), FinFunction([3,4],4))
 @test apex(g) == g0
 @test dom.(legs(g)) == [Graph(1), Graph(2)]
 @test feet(g) == [FinSet(1), FinSet(2)]
@@ -23,7 +23,7 @@ g = OpenGraph(g0, Cospan(FinFunction([1], 4), FinFunction([3,4], 4)))
 # Opposite of previous graph.
 h0 = Graph(4)
 add_edges!(h0, [1,2,3], [3,3,4])
-h = OpenGraph(h0, Cospan(FinFunction([1,2], 4), FinFunction([4], 4)))
+h = OpenGraph(h0, FinFunction([1,2],4), FinFunction([4],4))
 
 # Composition.
 k = compose(g, h)
@@ -56,16 +56,58 @@ const OpenWeightedGraphOb, OpenWeightedGraph = OpenACSetTypes(WeightedGraph, :V)
 
 g0 = WeightedGraph{Float64}(2)
 add_edge!(g0, 1, 2, weight=1.5)
-g = OpenWeightedGraph{Float64}(g0, Cospan(FinFunction([1],2), FinFunction([2],2)))
+g = OpenWeightedGraph{Float64}(g0, FinFunction([1],2), FinFunction([2],2))
 @test dom.(legs(g)) == [WeightedGraph{Float64}(1), WeightedGraph{Float64}(1)]
 @test feet(g) == [FinSet(1), FinSet(1)]
 
 h0 = WeightedGraph{Float64}(3)
 add_edges!(h0, [1,1], [2,3], weight=[1.0,2.0])
-h = OpenWeightedGraph{Float64}(h0, Cospan(FinFunction([1],3), FinFunction([2,3],3)))
+h = OpenWeightedGraph{Float64}(h0, FinFunction([1],3), FinFunction([2,3],3))
 k = compose(g, h)
 k0 = apex(k)
-@test (src(k0), tgt(k0)) == ([1,2,2], [2,3,4])
+@test src(k0) == [1,2,2]
+@test tgt(k0) == [2,3,4]
 @test subpart(k0, :weight) == [1.5, 1.0, 2.0]
+
+# Attributed boundary
+#--------------------
+
+@present TheoryLabeledGraph <: TheoryGraph begin
+  Label::Data
+  vlabel::Attr(V,Label)
+  elabel::Attr(E,Label)
+end
+
+const LabeledGraph = ACSetType(TheoryLabeledGraph, index=[:src,:tgt])
+const OpenLabeledGraphOb, OpenLabeledGraph = OpenACSetTypes(LabeledGraph, :V)
+
+g0 = LabeledGraph{Symbol}()
+add_vertices!(g0, 2, vlabel=[:x,:y])
+add_edge!(g0, 1, 2, elabel=:f)
+g = OpenLabeledGraph{Symbol}(g0, FinFunction([1],2), FinFunction([2],2))
+
+h0 = LabeledGraph{Symbol}()
+add_vertices!(h0, 3, vlabel=[:y,:w,:z])
+add_edges!(h0, [1,1], [2,3], elabel=[:g,:h])
+h = OpenLabeledGraph{Symbol}(h0, FinFunction([1],3), FinFunction([2,3],3))
+lfoot, rfoot = feet(h)
+@test lfoot isa ACSet
+@test keys(lfoot.tables) == (:V,)
+@test nparts(lfoot, :V) == 1
+@test nparts(rfoot, :V) == 2
+@test subpart(lfoot, :vlabel) == [:y]
+@test subpart(rfoot, :vlabel) == [:w,:z]
+
+k = compose(g, h)
+k0 = apex(k)
+@test src(k0) == [1,2,2]
+@test tgt(k0) == [2,3,4]
+@test subpart(k0, :vlabel) == [:x, :y, :w, :z]
+@test subpart(k0, :elabel) == [:f, :g, :h]
+
+# Incompatible attributes.
+set_subpart!(h0, 1, :vlabel, :y′)
+h = OpenLabeledGraph{Symbol}(h0, FinFunction([1],3), FinFunction([2,3],3))
+@test_throws ErrorException compose(g, h)
 
 end
