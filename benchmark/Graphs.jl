2011a46f52cd9a5ddde82c012aa056ddd7d08240
Evan Patterson <evan@epatters.org>
2020-11-18 18:27:50 -0800

    PERF: Benchmarks for making path (symmetric) graphs of length 500.

--- benchmark/Graphs.jl
+++ benchmark/Graphs.jl
@@ -11,20 +11,57 @@ const LG = LightGraphs
 # Graphs
 ########
 
-graphs = SUITE["Graph"] = BenchmarkGroup()
+bench = SUITE["Graph"] = BenchmarkGroup()
 
-graphs["make-path-graph"] = @benchmarkable begin
+n = 500
+bench["make-path"] = @benchmarkable begin
   g = Graph()
-  add_vertices!(g, 10)
-  add_edges!(g, 1:9, 2:10)
+  add_vertices!(g, n)
+  add_edges!(g, 1:(n-1), 2:n)
 end
 
-graphs["make-path-graph-lightgraphs"] = @benchmarkable begin
+bench["make-path-lightgraphs"] = @benchmarkable begin
   g = LG.DiGraph()
-  LG.add_vertices!(g, 10)
-  for v in 1:9
+  LG.add_vertices!(g, n)
+  for v in 1:(n-1)
     LG.add_edge!(g, v, v+1)
   end
 end
+
+g = Graph(5)
+add_edges!(g, [1,2,3,3], [3,3,4,5])
+bench["inneighbors"] = @benchmarkable inneighbors($g, 3)
+bench["outneighbors"] = @benchmarkable outneighbors($g, 3)
+
+lg = LG.DiGraph(g)
+bench["inneighbors-lightgraphs"] = @benchmarkable LG.inneighbors($lg, 3)
+bench["outneighbors-lightgraphs"] = @benchmarkable LG.outneighbors($lg, 3)
+
+# Symmetric graphs
+##################
+
+bench = SUITE["SymmetricGraph"] = BenchmarkGroup()
+
+n = 500
+bench["make-path"] = @benchmarkable begin
+  g = SymmetricGraph()
+  add_vertices!(g, n)
+  add_edges!(g, 1:(n-1), 2:n)
+end
+
+bench["make-path-lightgraphs"] = @benchmarkable begin
+  g = LG.Graph()
+  LG.add_vertices!(g, n)
+  for v in 1:(n-1)
+    LG.add_edge!(g, v, v+1)
+  end
+end
+
+g = SymmetricGraph(5)
+add_edges!(g, [1,2,3,3], [3,3,4,5])
+bench["neighbors"] = @benchmarkable neighbors($g, 3)
+
+lg = LG.Graph(g)
+bench["neighbors-lightgraphs"] = @benchmarkable LG.neighbors($lg, 3)
 
 end
