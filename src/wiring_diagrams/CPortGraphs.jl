91c4f6bd88cfa1db303dbd97018854c8692cc4fb
James Fairbanks <jpfairbanks@gmail.com>
2020-11-22 12:36:41 -0500

    ENH: add Operad of BundleCPGs

--- src/wiring_diagrams/CPortGraphs.jl
+++ src/wiring_diagrams/CPortGraphs.jl
@@ -120,7 +120,75 @@ function ocompose(g::OpenCPortGraph, xs::Vector)
         ι_tgtport = legs(u.cocone)[tbox][:P](localport_tgt)
         add_part!(sum, :W; src=ι_srcport, tgt=ι_tgtport) 
     end
+    rem_parts!(sum, :OP, parts(xsum, :OP))
+    for op in parts(g, :OP)
+        i = g[op, [:con, :box]]
+        localport = findfirst(op .== incident(g, i, :box))
+        newop = legs(u.cocone)[i][:P](localport)
+        add_parts!(sum, :OP, 1, con=newop)
+    end
     return sum
 end
+
+@present ThBundledCPG <: ThOpenCPortGraph begin
+    Bundle::Ob
+    bun::Hom(OP, Bundle)
+end
+
+const BundledCPG = ACSetType(ThBundledCPG)
+
+migrate!(b::BundledCPG, g::OpenCPortGraph) = migrate!(b,g,
+    Dict(:B=>:B, :P=>:P, :W=>:W, :OP=>:OP, :Bundle=>:OP),
+    Dict(:src=>:src, :tgt=>:tgt, :box=>:box,
+         :con=>:con, :bun=>id(ThOpenCPortGraph[:OP]))
+)
+
+function BundledCPG(g::OpenCPortGraph)
+    bg = BundledCPG()
+    copy_parts!(bg, g, 
+      W=parts(g,:W), P = parts(g,:P), B=parts(g, :B), OP=parts(g,:OP))
+    return bg
+end
+
+function oapply(g::BundledCPG, xs::Vector)
+    u = coproduct(xs)
+    xsum=apex(u)
+    for e in parts(g, :W)
+        s = g[e,:src]
+        t = g[e,:tgt]
+        sbox = g[s, :box]
+        tbox = g[t, :box]
+
+        localport_src = findfirst(s .== incident(g, sbox, :box))
+        localport_tgt = findfirst(t .== incident(g, tbox, :box))
+        @assert length(findall(s .== incident(g, sbox, :box))) == 1
+        @assert length(findall(t .== incident(g, tbox, :box))) == 1
+
+        # println("$s:$sbox→$t:$tbox")
+        sbun = incident(xs[sbox], localport_src, :bun)
+        # println("sbun: $sbun")
+        tbun = incident(xs[tbox], localport_tgt, :bun)
+        # println("tbun: $tbun")
+        for thread in zip(sbun, tbun)
+            ι_srcport = legs(u.cocone)[sbox][:P](xs[sbox][thread[1], :con])
+            ι_tgtport = legs(u.cocone)[tbox][:P](xs[tbox][thread[2], :con])
+            # println("adding:\t $ι_srcport, $ι_tgtport")
+            add_part!(xsum, :W; src=ι_srcport, tgt=ι_tgtport) 
+        end
+    end
+    rem_parts!(xsum, :OP, parts(xsum, :OP))
+    rem_parts!(xsum, :Bundle, parts(xsum, :Bundle))
+    add_parts!(xsum, :Bundle, nparts(g, :Bundle))
+    for op in parts(g, :OP)
+        i = g[op, [:con, :box]]
+        localport = findfirst(op .== incident(g, i, :box))
+        @assert length(findall(op .== incident(g, i, :box))) == 1
+        newop = legs(u.cocone)[i][:P](incident(xs[i], localport, :bun))
+        # println("bundling: $op: $localport: $newop")
+        add_parts!(xsum, :OP, length(newop), con=newop, bun=op)
+    end
+    return xsum
+end
+
 
 end
--- test/wiring_diagrams/CPortGraphs.jl
+++ test/wiring_diagrams/CPortGraphs.jl
@@ -66,3 +66,43 @@ end
   @test subpart(g′, :, :tgt) == [2, 4, 6, 3, 5, 1, 8, 10, 12, 9, 11, 7, 7, 8, 9, 10, 11, 12] 
 end
 
+
+@testset "Bundling CPGs" begin
+  
+EdgeGraph() = begin
+  g = Graph()
+  add_parts!(g, :V, 2)
+  add_parts!(g, :E, 1, src=[1], tgt=[2])
+  return g
+  end
+  
+  e₁ = EdgeGraph()
+  oe₁ = migrate!(OpenCPortGraph(), (migrate!(CPortGraph(), e₁)))
+  @test subpart(oe₁, :con) == 1:2
+  mboe₁ = migrate!(BundledCPG(), oe₁)
+  @test subpart(mboe₁, :bun) == 1:2
+  
+  boe₁ = BundledCPG(oe₁)
+  add_parts!(boe₁, :Bundle, 1)
+  set_subpart!(boe₁, :bun, [1,1])
+  boe₁
+  
+  f = OpenCPortGraph(migrate!(CPortGraph(), e₁))
+  f = BundledCPG(f)
+  fee = oapply(f, [boe₁, boe₁])
+  @test fee[:, :src] == [1,3,1,2]
+  @test fee[:, :tgt] == [2,4,3,4]
+  
+  add_parts!(f, :Bundle, 2)
+  add_parts!(f, :OP, 2, con=[1,2], bun=[1,2])
+  fee′ = oapply(f, [boe₁, boe₁])
+  @test fee′[:, :con] == 1:4
+  @test fee′[:, :bun] == [1,1,2,2]
+  h = oapply(f, [fee′, fee′])
+  @test nparts(h, :W) == 10
+  
+  set_subpart!(fee′, :bun, 1)
+  @show fee′
+  h = oapply(f, [fee′, fee′])
+  @test nparts(h, :W) == 12
+end
