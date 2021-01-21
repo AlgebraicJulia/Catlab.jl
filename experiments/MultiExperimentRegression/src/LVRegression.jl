ebb1580749ccd9d64960a423ac6e7bf429ea374c
Owen Lynch <root@owenlynch.org>
2021-01-21 16:35:47 -0500

    Regression for rates of a petri net

--- experiments/MultiExperimentRegression/src/LVRegression.jl
+++ experiments/MultiExperimentRegression/src/LVRegression.jl
@@ -21,4 +21,39 @@ prob1 = ODEProblem(lotka_volterra,u0,(0.0,10.0),p)
 sol = solve(prob1,Tsit5())
 plot(sol)
 
+sol1 = solve(prob1,Tsit5(),saveat=0.1)
+odedata = Array(sol1) + 0.8 * randn(size(Array(sol1)))
+plot(sol1, alpha = 0.3, legend = false); scatter!(sol1.t, odedata')
+
+Turing.setadbackend(:forwarddiff)
+
+@model function fitlv(data, prob1)
+  σ ~ InverseGamma(2, 3) # ~ is the tilde character
+
+  p ~ product_distribution([Uniform(0,5) for i in 1:4])
+  
+  prob = remake(prob1, p=p)
+  predicted = solve(prob,Tsit5(),saveat=0.1)
+
+  for i = 1:length(predicted)
+    data[:,i] ~ MvNormal(predicted[i], σ)
+  end
 end
+
+model = fitlv(odedata, prob1)
+
+# This next command runs 3 independent chains without using multithreading. 
+chain = mapreduce(c -> sample(model, NUTS(.65),1000), chainscat, 1:3)
+
+
+@model function gdemo(x)
+  s ~ InverseGamma(2,3)
+  m ~ Normal(0, sqrt(s))
+
+  for i in eachindex(x)
+    x[i] ~ Normal(m, sqrt(s))
+  end
+end
+
+model = gdemo([missing, missing])
+c = sample(model, HMC(0.01, 5), 500)
--- experiments/MultiExperimentRegression/src/PetriRegression.jl
+++ experiments/MultiExperimentRegression/src/PetriRegression.jl
@@ -1,8 +1,12 @@
 module PetriRegression
 
+using Turing, Distributions, DifferentialEquations
+
+using MCMCChains, Plots, StatsPlots
+
 using AlgebraicPetri
-using Turing
-using DifferentialEquations
+
+using Catlab.CategoricalAlgebra.CSets
 
 struct ReactionData
   dt :: Float64
@@ -14,21 +18,35 @@ end
 
 Turing.setadbackend(:forwarddiff)
 
-@model function fit_rates(net::Petri, data::ReactionData)
+@model function fit_rates(measurements, prob1)
   σ ~ InverseGamma(2,3)
-  vf = vectorfield(net)
-  t0,t1 = 0., size(data.measurements,1) * data.dt
-  p = Vector{Float64}(undef,nt(net))
-  for i in 1:nt(net)
-    p[i] ~ Uniform(data.priors[i]...)
-  end
-  prob = ODEProblem(vf,data.measurements[1,:],(t0,t1),p)
-  predicted = solve(prob,Tsit5(),saveat=data.dt)
+
+  p ~ product_distribution([Uniform(0,10) for i in 1:3])
+
+  prob = remake(prob1,p=p)
+  predicted = solve(prob,Tsit5(),saveat=0.1)
 
   for i = 1:length(predicted)
-    data.measurements[i,:] ~ MvNormal(predicted[i], σ)
+    measurements[:,i] ~ MvNormal(predicted[i], σ)
   end
 end
   
+lv = PetriNet(2,(1,(1,1)),((1,2),(2,2)),(2,()))
+
+u0 = [1.,1.]
+p = [1.,2.,3.]
+prob = ODEProblem(vectorfield(lv),u0,(0.,10.),p)
+sol = solve(prob,Tsit5(),saveat=0.1)
+
+plot(sol)
+
+σ = 0.2
+dt = 0.1
+measurements = Array(sol) + σ * randn(size(Array(sol)))
+plot(sol, alpha=0.5, legend = false); scatter!(sol.t, measurements')
+priors = [(0.,5.),(0.,5.),(0.,5.)]
+
+model = fit_rates(measurements, prob)
+sample(model,HMC(0.1,5),1000)
 
 end
