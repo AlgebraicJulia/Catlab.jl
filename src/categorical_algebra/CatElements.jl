b6eced7f672310be17f341cd555655b807e582de
James P. Fairbanks <fairbanksj@ufl.edu>
2021-07-29 10:43:17 -0400

    fix indent

--- src/categorical_algebra/CatElements.jl
+++ src/categorical_algebra/CatElements.jl
@@ -11,16 +11,16 @@ export ThElements, AbstractElements, Elements, elements
 
 
 @present ThElements(FreeSchema) begin
-    (El, Arr, Ob, Hom)::Ob
-    Name::Data
-    src::Hom(Arr, El)
-    tgt::Hom(Arr, El)
-    dom::Hom(Hom, Ob)
-    cod::Hom(Hom, Ob)
-    πₑ::Hom(El, Ob)
-    πₐ::Hom(Arr, Hom)
-    nameo::Attr(Ob, Name)
-    nameh::Attr(Hom, Name)
+  (El, Arr, Ob, Hom)::Ob
+  Name::Data
+  src::Hom(Arr, El)
+  tgt::Hom(Arr, El)
+  dom::Hom(Hom, Ob)
+  cod::Hom(Hom, Ob)
+  πₑ::Hom(El, Ob)
+  πₐ::Hom(Arr, Hom)
+  nameo::Attr(Ob, Name)
+  nameh::Attr(Hom, Name)
 end
 
 const AbstractElements = AbstractACSetType(ThElements)
@@ -34,49 +34,49 @@ homomorphism is a graph shaped like the schema. This is one half of the isomorph
 databases and knowledge graphs.
 """
 function elements(X::ACS) where
-    {CD <: CatDesc, AD <: AttrDesc{CD}, Ts, ACS <: AbstractACSet{CD,AD,Ts}}
-    Y = Elements{Symbol}()
+  {CD <: CatDesc, AD <: AttrDesc{CD}, Ts, ACS <: AbstractACSet{CD,AD,Ts}}
+  Y = Elements{Symbol}()
 
-    add_parts!(Y, :Ob, length(ob(CD)), nameo=ob(CD))
-    els = map(enumerate(ob(CD))) do (i,c)
-        add_parts!(Y, :El, nparts(X, c), πₑ = i)
-    end
+  add_parts!(Y, :Ob, length(ob(CD)), nameo=ob(CD))
+  els = map(enumerate(ob(CD))) do (i,c)
+    add_parts!(Y, :El, nparts(X, c), πₑ = i)
+  end
 
-    add_parts!(Y, :Hom, length(hom(CD)), dom=dom(CD), cod=codom(CD), nameh=hom(CD))
-    map(enumerate(zip(hom(CD), dom(CD), codom(CD)))) do (i, trip)
-        f, ci, di = trip
-        c = ob(CD)[ci]
-        d = ob(CD)[di]
-        nc = nparts(X, c)
-        add_parts!(Y, :Arr, nparts(X, c), src = els[ci], tgt = view(els[di], X[f]), πₐ = i)
-    end
-    return Y
+  add_parts!(Y, :Hom, length(hom(CD)), dom=dom(CD), cod=codom(CD), nameh=hom(CD))
+  map(enumerate(zip(hom(CD), dom(CD), codom(CD)))) do (i, trip)
+    f, ci, di = trip
+    c = ob(CD)[ci]
+    d = ob(CD)[di]
+    nc = nparts(X, c)
+    add_parts!(Y, :Arr, nparts(X, c), src = els[ci], tgt = view(els[di], X[f]), πₐ = i)
+  end
+  return Y
 end
 
 
 function presentation(X::AbstractElements)
-    P = Presentation(FreeSchema)
-    obs = Dict{Tuple{Symbol, Int}, Any}()
-    for a in 1:nparts(X, :Ob)
-        class = X[a, :nameo]
-        for (i,j) in enumerate(incident(X, a, :πₑ))
-            ob = Ob(FreeSchema, Symbol("$(class)_$i"))
-            obs[(class, j)] = ob
-        end
+  P = Presentation(FreeSchema)
+  obs = Dict{Tuple{Symbol, Int}, Any}()
+  for a in 1:nparts(X, :Ob)
+    class = X[a, :nameo]
+    for (i,j) in enumerate(incident(X, a, :πₑ))
+      ob = Ob(FreeSchema, Symbol("$(class)_$i"))
+      obs[(class, j)] = ob
     end
-    add_generators!(P, values(obs))
-    homs = Dict{Tuple{Symbol, Int}, Any}()
-    for f in 1:nparts(X, :Hom)
-        class = X[f, :nameh]
-        for (i,j) in enumerate(incident(X, f, :πₐ))
-            domkey = (X[j, [:src, :πₑ, :nameo]], X[j, :src])
-            dom = obs[domkey]
-            codkey = (X[j, [:tgt, :πₑ, :nameo]], X[j, :tgt])
-            codom = obs[codkey]
-            homs[(class, j)] = Hom(Symbol("$(class)_$dom"), dom, codom)
-        end
+  end
+  add_generators!(P, values(obs))
+  homs = Dict{Tuple{Symbol, Int}, Any}()
+  for f in 1:nparts(X, :Hom)
+    class = X[f, :nameh]
+    for (i,j) in enumerate(incident(X, f, :πₐ))
+      domkey = (X[j, [:src, :πₑ, :nameo]], X[j, :src])
+      dom = obs[domkey]
+      codkey = (X[j, [:tgt, :πₑ, :nameo]], X[j, :tgt])
+      codom = obs[codkey]
+      homs[(class, j)] = Hom(Symbol("$(class)_$dom"), dom, codom)
     end
-    add_generators!(P, values(homs))
-    return P, obs, homs
+  end
+  add_generators!(P, values(homs))
+  return P, obs, homs
 end
 end
