e20701b78c568799edd5109b290b9b5992afe670
Evan Patterson <evan@epatters.org>
2021-08-19 15:52:06 -0700

    REFACTOR: Type representing sub-C-set componentwise with subsets.

--- test/categorical_algebra/CSets.jl
+++ test/categorical_algebra/CSets.jl
@@ -344,20 +344,22 @@ h = cycle_graph(LabeledGraph{Symbol}, 4, V=(label=[:a,:b,:d,:c],))
 # Construct sub-C-sets.
 X = path_graph(Graph, 4)
 A = Subobject(X, V=[2,3,4], E=[2,3])
-@test Subobject(X, V=[false,true,true,true], E=[false,true,true]) == A
+@test ob(A) == X
 α = hom(A)
 @test is_natural(α)
 @test dom(α) == path_graph(Graph, 3)
 @test codom(α) == X
+A_pred = Subobject(X, V=[false,true,true,true], E=[false,true,true])
+@test hom(A_pred) == α
 
 # Lattice of sub-C-sets.
 X = Graph(6)
 add_edges!(X, [1,2,3,4,4], [3,3,4,5,6])
 A, B = Subobject(X, V=1:4, E=1:3), Subobject(X, V=3:6, E=3:5)
-@test A ∧ B == Subobject(X, V=3:4, E=3:3)
-@test A ∨ B == Subobject(X, V=1:6, E=1:5)
-@test ⊤(X) |> force == Subobject(X, V=1:6, E=1:5)
-@test ⊥(X) |> force == Subobject(X, V=1:0, E=1:0)
+@test A ∧ B |> force == Subobject(X, V=3:4, E=3:3) |> force
+@test A ∨ B |> force == Subobject(X, V=1:6, E=1:5) |> force
+@test ⊤(X) |> force == Subobject(X, V=1:6, E=1:5) |> force
+@test ⊥(X) |> force == Subobject(X, V=1:0, E=1:0) |> force
 
 # Bi-Heyting algebra of sub-C-sets.
 #
@@ -371,12 +373,12 @@ Z = cycle_graph(Graph, 1) ⊕ cycle_graph(Graph, 1)
                          CSetTransformation(I, Z, V=[1]))
 B_implies_C, B = Subobject(ιY), Subobject(ιZ)
 C = Subobject(ob(colim), V=2:5, E=2:3)
-@test (B ⟹ C) == B_implies_C
+@test (B ⟹ C) |> force == B_implies_C |> force
 
 # Subtraction in Graph (Reyes et al 2004, Sec 9.1, p. 144).
 X = ob(colim)
 C = Subobject(X, V=2:5, E=[2,3,ne(X)-1])
-@test (B \ C) == Subobject(X, V=[nv(X)], E=[ne(X)])
+@test (B \ C) |> force == Subobject(X, V=[nv(X)], E=[ne(X)]) |> force
 
 # Negation in Graph (Reyes et al 2004, Sec 9.1, p. 139-140).
 X = cycle_graph(Graph, 1) ⊕ path_graph(Graph, 2) ⊕ cycle_graph(Graph, 4)
@@ -385,15 +387,15 @@ add_edge!(X, 4, 8)
 A = Subobject(X, V=[2,3,4,5,8], E=[3,7])
 neg_A = Subobject(X, V=[1,6,7], E=[1,5])
 @test is_natural(hom(A)) && is_natural(hom(neg_A))
-@test ¬A == neg_A
-@test ¬neg_A == Subobject(X, V=[2,3,4,5,8], E=[2,3,7])
+@test ¬A |> force == neg_A |> force
+@test ¬neg_A |> force == Subobject(X, V=[2,3,4,5,8], E=[2,3,7]) |> force
 
 # Non in Graph (Reyes et al 2004, Sec 9.1, p. 144).
 X = path_graph(Graph, 5) ⊕ path_graph(Graph, 2) ⊕ cycle_graph(Graph, 1)
 A = Subobject(X, V=[1,4,5], E=[4])
 non_A = Subobject(X, V=setdiff(vertices(X), 5), E=setdiff(edges(X), 4))
-@test ~A == non_A
-@test ~non_A == Subobject(X, V=[4,5], E=[4])
+@test ~A |> force == non_A |> force
+@test ~non_A |> force == Subobject(X, V=[4,5], E=[4]) |> force
 
 # Negation and non in DDS.
 S₁ = DDS(); add_parts!(S₁, :X, 5, Φ=[3,3,4,5,5])
@@ -401,9 +403,9 @@ S₂ = DDS(); add_parts!(S₂, :X, 3, Φ=[2,3,3])
 ι₁, ι₂ = colim = coproduct(S₁, S₂)
 S = ob(colim)
 A = Subobject(S, X=[3,4,5])
-@test ¬A == Subobject(ι₂)
-@test ¬Subobject(ι₂) == Subobject(ι₁)
-@test ~A == ⊤(S) |> force
+@test ¬A |> force == Subobject(ι₂)
+@test ¬Subobject(ι₂) |> force == Subobject(ι₁)
+@test ~A |> force == ⊤(S) |> force
 
 # Serialization
 ###############
