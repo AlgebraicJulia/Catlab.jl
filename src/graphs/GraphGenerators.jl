3ac1f4f3345e9f9682a923e4fc552bf3a6e2c888
Evan Patterson <evan@epatters.org>
2021-10-03 13:51:20 -0700

    ENH: Convenience function to construct graph with parallel edges.

--- src/graphs/GraphGenerators.jl
+++ src/graphs/GraphGenerators.jl
@@ -1,5 +1,6 @@
 module GraphGenerators
-export path_graph, cycle_graph, complete_graph, star_graph, wheel_graph
+export path_graph, cycle_graph, complete_graph, star_graph, wheel_graph,
+  parallel_arrows
 
 using ...CSetDataStructures, ..BasicGraphs
 using ...CSetDataStructures: hom
@@ -56,6 +57,15 @@ function wheel_graph(::Type{T}, n::Int) where T <: ACSet
   g = cycle_graph(T, n-1)
   add_vertex!(g)
   add_edges!(g, fill(n,n-1), 1:(n-1))
+  g
+end
+
+""" Graph with two vertices and ``n`` parallel edges.
+"""
+function parallel_arrows(::Type{T}, n::Int; V=(;), E=(;)) where T <: ACSet
+  g = T()
+  add_vertices!(g, 2; V...)
+  add_edges!(g, fill(1,n), fill(2,n); E...)
   g
 end
 
