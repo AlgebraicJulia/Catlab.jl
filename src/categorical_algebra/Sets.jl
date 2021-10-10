0a756fc6d528f1cf250d7bbc812c2734b19cb991
Evan Patterson <evan@epatters.org>
2021-10-10 15:55:11 -0700

    DOC: Fix/remove some broken cross-references.

--- src/categorical_algebra/Sets.jl
+++ src/categorical_algebra/Sets.jl
@@ -2,7 +2,7 @@
 
 This module defines generic types for the category of sets ([`SetOb`](@ref),
 [`SetFunction`](@ref)), as well as a few basic concrete types, such as a wrapper
-type to view Julia types as sets ([`TypeOb`](@ref)). Extensive support for
+type to view Julia types as sets ([`TypeSet`](@ref)). Extensive support for
 finite sets is provided by another module, [`FinSets`](@ref).
 """
 module Sets
--- src/graphs/PropertyGraphs.jl
+++ src/graphs/PropertyGraphs.jl
@@ -36,7 +36,7 @@ const _PropertyGraph{T} = __PropertyGraph{Dict{Symbol,T}}
 "Property graphs" are graphs with arbitrary named properties on the graph,
 vertices, and edges. They are intended for applications with a large number of
 ad-hoc properties. If you have a small number of known properties, it is better
-and more efficient to create a specialized C-set type using [`CSetType`](@ref).
+and more efficient to create a specialized C-set type using `@acset_type`.
 
 See also: [`SymmetricPropertyGraph`](@ref).
 """
