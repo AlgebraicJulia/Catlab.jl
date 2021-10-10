1cd367481bf8f3d0213c4d32fcf463f384c5e7b5
Evan Patterson <evan@epatters.org>
2021-10-10 15:45:43 -0700

    DOC: Brief overview of the `Programs` module.

--- src/programs/ParseJuliaPrograms.jl
+++ src/programs/ParseJuliaPrograms.jl
@@ -12,17 +12,16 @@ using ...Theories: ObExpr, HomExpr, otimes, munit
 using ...WiringDiagrams
 using ..GenerateJuliaPrograms: make_return_value
 
-
 """ Parse a wiring diagram from a Julia program.
 
-For the most part, this is standard Julia code but we take a few liberties with
-the syntax. Products are represented as tuples. So if `x` and `y` are variables
-of type \$X\$ and \$Y\$, then `(x,y)` has type \$X \\otimes Y\$. Also, both `()`
-and `nothing` are interpreted as the monoidal unit \$I\$.
+For the most part, this is standard Julia code but a few liberties are taken
+with the syntax. Products are represented as tuples. So if `x` and `y` are
+variables of type ``X`` and ``Y``, then `(x,y)` has type ``X ⊗ Y``. Also, both
+`()` and `nothing` are interpreted as the monoidal unit ``I``.
 
-Unlike in standard Julia, the call expressions `f(x,y)` and `f((x,y))` are
-equivalent. Consequently, given morphisms \$f: W \\to X \\otimes Y\$ and \$g: X
-\\otimes Y \\to Z\$, the code
+Unlike standard Julia, the function call expressions `f(x,y)` and `f((x,y))` are
+equivalent. Consequently, given morphisms ``f: W → X ⊗ Y`` and ``g: X ⊗ Y → Z``,
+the code
 
 ```julia
 x, y = f(w)
@@ -32,12 +31,12 @@ g(x,y)
 is equivalent to `g(f(w))`. In standard Julia, at most one of these calls to `g`
 would be valid, unless `g` had multiple signatures.
 
-The diagonals (copying and deleting) are implicit in the Julia syntax: copying
-is variable reuse and deleting is variable non-use. For the codiagonals (merging
-and creating), a special syntax is provided, reinterpreting Julia's vector
-literals. The merge of `x1` and `x2` is represented by the vector `[x1,x2]` and
-creation by the empty vector `[]`. For example, `f([x1,x2])` translates to
-`compose(mmerge(X),f)`.
+The diagonals (copying and deleting) of a cartesian category are implicit in the
+Julia syntax: copying is variable reuse and deleting is variable non-use. For
+the codiagonals (merging and creating), a special syntax is provided,
+reinterpreting Julia's vector literals. The merging of `x1` and `x2` is
+represented by the vector `[x1,x2]` and creation by the empty vector `[]`. For
+example, `f([x1,x2])` translates to `compose(mmerge(X),f)`.
 
 This macro is a wrapper around [`parse_wiring_diagram`](@ref).
 """
