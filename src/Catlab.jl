76294da9d285d944a001768fd3c78e768e845c22
Kris <ksb@stanford.edu>
2021-11-09 15:19:54 -0800

    Remove LinearAlgebra
    Most moved to GraphicalLinearAlgebra. TensorNetworks moved to expt
    Remove LinearAlgebra

--- src/Catlab.jl
+++ src/Catlab.jl
@@ -13,6 +13,5 @@ include("categorical_algebra/CategoricalAlgebra.jl")
 include("wiring_diagrams/WiringDiagrams.jl")
 include("graphics/Graphics.jl")
 include("programs/Programs.jl")
-include("linear_algebra/LinearAlgebra.jl")
 
 end
