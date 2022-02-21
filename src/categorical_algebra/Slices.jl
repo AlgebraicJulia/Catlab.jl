945d69ba5f306a182b383f0e5f3b56a914257295
Kris <ksb@stanford.edu>
2022-02-21 08:53:04 -0500

    Colimits / pushout complement of slices. Direct rewriting of slices

--- src/categorical_algebra/Slices.jl
+++ src/categorical_algebra/Slices.jl
@@ -1,36 +1,37 @@
 module Slices
-export Slice, SliceMorphism,SliceLimit
+export Slice, SliceMorphism,SliceLimit,SliceColimit, pushout_complement
 
 using ...GAT
 using ..FreeDiagrams, ..Limits, ..CSets
 using ...Theories: Category
 import ...Theories: dom, codom, compose, id
-import ..Limits: limit, universal
-import ..FinSets: force
+import ..Limits: limit, colimit, universal
+import ..FinSets: force, pushout_complement
 
 struct Slice{Ob,Hom}
-    slice::Hom
+  slice::Hom
 end
 
 struct SliceMorphism{Ob, Hom}
   dom::Slice{Ob, Hom}
   codom::Slice{Ob, Hom}
   f::Hom
-  function SliceMorphism{Ob,Hom}(dom::Slice{Ob, Hom},codom::Slice{Ob, Hom},
-                                 f::Hom) where {Ob, Hom}
+  function SliceMorphism{Ob, Hom}(dom, codom, f) where {Ob, Hom}
     p1, p2 = force.([dom.slice, compose(f,codom.slice)])
     force(p1) == force(p2) ||
        error("Slice condition failed: \ndom.slice $p1\ncomposite $p2\nf " *
              "$(force(f))\ncodom $(force(codom.slice))")
-    return new(dom, codom, f)
+    return new{Ob,Hom}(dom, codom, f)
   end
 end
+
+SliceMorphism(a,b,f) = SliceMorphism{typeof(a).parameters...}(a,b,f)
 
 
 dom(s::Slice)::StructACSet = dom(s.slice)
 codom(s::Slice)::StructACSet = codom(s.slice)
 force(s::Slice{Ob,Hom}) where {Ob,Hom} = Slice{Ob,Hom}(force(s.slice))
-force(s::SliceMorphism{Ob,Hom}) where {Ob,Hom} = SliceMorphism{Ob,Hom}(
+force(s::SliceMorphism{Ob,Hom}) where {Ob,Hom} = SliceMorphism(
   force(dom(s)), force(codom(s)), force(s.f))
 
 
@@ -40,6 +41,15 @@ struct SliceLimit{Ob_, Hom, Ob <: Slice{Ob_,Hom}, Diagram,
   cone::Cone
   underlying::AbstractLimit
 end
+
+struct SliceColimit{Ob_, Hom, Ob <: Slice{Ob_,Hom}, Diagram,
+                    Cocone <: Multicospan{Ob}} <: AbstractColimit{Ob,Diagram}
+  diagram::Diagram
+  cocone::Cocone
+  underlying::AbstractColimit
+end
+
+
 
 
 @instance Category{Slice, SliceMorphism} begin
@@ -70,23 +80,57 @@ end
 Convert a limit problem in the slice category to a limit problem of the
 underlying category.
 """
+limit(csp::Multicospan{<:Slice{Ob,Hom}}) where {Ob,Hom} = limit(FreeDiagram{Ob, Hom}(csp))
+
 function limit(::Type{Tuple{Slice{Ob, Hom}, SliceMorphism{Ob, Hom}}},
                diagram::FreeDiagram{Slice{Ob, Hom}, SliceMorphism{Ob, Hom}}
                ) where {Ob, Hom}
   lim = limit(slice_diagram(diagram))
   new_apex = Slice{Ob,Hom}(first(legs(lim.cone)))
-  new_cone_legs = [SliceMorphism{Ob,Hom}(new_apex, ob, leg) for (ob, leg)
+  new_cone_legs = [SliceMorphism(new_apex, ob, leg) for (ob, leg)
                    in zip(diagram[:ob],legs(lim.cone)[2:end])]
   return SliceLimit(diagram, Multispan(new_apex, new_cone_legs), lim)
 end
 
+"""Warning: requires nonempty diagram in order to know what is sliced over"""
+colimit(sp::Multispan{<:Slice{Ob,Hom}}) where {Ob,Hom} = colimit(FreeDiagram{Slice{Ob,Hom}, SliceMorphism{Ob,Hom}}(sp))
+colimit(::Type{Tuple{SliceMorphism{Ob, Hom}, Any}},
+                 p::ObjectPair{<:SliceMorphism}) where {Ob, Hom} =
+  colimit(Span(p[1], p[2]))
+
+function colimit(::Type{Tuple{Slice{Ob, Hom}, SliceMorphism{Ob, Hom}}},
+                  diagram::FreeDiagram{Slice{Ob, Hom}, SliceMorphism{Ob, Hom}}) where {Ob, Hom}
+  # discard all the slice info in the colimit diagram - it's irrelevant
+  obs  = [x.slice for x in diagram[:ob]]
+  obdoms = dom.(obs)
+  X = codom(first(diagram[:ob]))
+  homs = [(h.f, s, t) for (h, s, t) in zip(diagram[:hom], diagram[:src], diagram[:tgt])]
+  colim = colimit(FreeDiagram(obdoms,homs))
+  csp = Multicospan(X, obs)
+  new_apex = Slice{Ob, Hom}(universal(colim, csp))
+  new_cocone_legs = [SliceMorphism(o, new_apex, l)
+                     for (o, l) in zip(diagram[:ob],legs(colim))]
+  # compute new apex using the universal property of colimits
+  return SliceColimit(diagram, Multicospan(new_apex, new_cocone_legs), colim)
+end
+
 """Use the universal property of the underlying category"""
-function universal(lim::SliceLimit{Ob,Hom}, sp::Multispan) where {Ob, Hom}
+function universal(lim::SliceLimit{Ob,Hom}, sp::Multispan) where{Ob, Hom}
   apx = apex(sp)
   newspan = vcat([apx.slice],[x.f for x in sp])
   u = universal(lim.underlying, Multispan(dom(apx), newspan))
   apx2 = Slice{Ob,Hom}(first(legs(lim.underlying.cone)))
-  return SliceMorphism{Ob,Hom}(apx, apx2, u)
+  return SliceMorphism(apx, apx2, u)
+end
+
+
+function pushout_complement(f::SliceMorphism{Ob, Hom}, g::SliceMorphism{Ob, Hom}
+    )::Pair{SliceMorphism{Ob, Hom}, SliceMorphism{Ob, Hom}} where {S, Ob <: StructACSet{S}, Hom <: ACSetTransformation}
+    f_, g_ = pushout_complement(ComposablePair(f.f, g.f))
+    D__ = Slice{Ob, Hom}(compose(g_, codom(g).slice))
+    g__ = SliceMorphism(D__, codom(g), g_)
+    f__ = SliceMorphism(dom(f), D__, f_)
+    return f__ => g__
 end
 
 end # module
--- test/categorical_algebra/Slices.jl
+++ test/categorical_algebra/Slices.jl
@@ -25,18 +25,46 @@ that maps to (a₂,b₁).
 G2,G1,G3 = Graph.([2,1,3])
 GraphSlice = Slice{Graph, ACSetTransformation}
 GraphSliceMorphism = SliceMorphism{Graph, ACSetTransformation}
-AB = GraphSlice.([ACSetTransformation(G2, G3;V=x) for x in [[1,2],[2,1]]])
-slice_dia = FreeDiagram(DiscreteDiagram(AB, GraphSliceMorphism))
+A, B = GraphSlice.([ACSetTransformation(G2, G3;V=x) for x in [[1,2],[2,1]]])
+slice_dia = FreeDiagram(DiscreteDiagram([A,B], GraphSliceMorphism))
 slice_lim = limit(slice_dia)
 
 
-span_morphisms = [ACSetTransformation(G1,G2;V=x) for x in [[1],[2]]]
+mA, mB = [ACSetTransformation(G1,G2;V=x) for x in [[1],[2]]]
 map_to_base = GraphSlice(ACSetTransformation(G1,G3;V=[1]))
-toA, toB = [GraphSliceMorphism(map_to_base, ab, span_morphism)
-          for (span_morphism, ab) in zip(span_morphisms, AB)]
+toA = GraphSliceMorphism(map_to_base, A, mA)
+toB = GraphSliceMorphism(map_to_base, B, mB)
 u = universal(slice_lim, Span(toA, toB))
 @test dom(u) == map_to_base
 @test codom(u) == apex(slice_lim)
 @test force(compose(u, legs(slice_lim)[1]).f) == force(toA.f)
 
+
+# Pushout complement in Petri, computed as slice in Grph
+two = @acset Graph begin V=2; E=2; src=[1,2]; tgt=[2,1] end
+a,b,c = Graph(1), path_graph(Graph, 2), path_graph(Graph, 3)
+A = GraphSlice(ACSetTransformation(a, two, V=[2])) # a transition
+B = GraphSlice(ACSetTransformation(b, two, V=[2,1], E=[2]))
+C = GraphSlice(ACSetTransformation(c, two, V=[1,2,1], E=[1,2]))
+ff = ACSetTransformation(a,b,V=[1])
+gg = ACSetTransformation(b,c,V=[2,3], E=[2])
+f = GraphSliceMorphism(A,B,ff)
+g = GraphSliceMorphism(B,C,gg)
+pc1, pc2 = pushout_complement(ComposablePair(ff,gg))
+p1, p2 = pushout_complement(f,g)
+
+# Colimit: a pushout of slices
+#------------------------------
+# do the pushout associated with the pushout complement above
+slice_dia = FreeDiagram{GraphSlice,GraphSliceMorphism}(Multispan(A, [f, p1]))
+clim = colimit(slice_dia)
+@test is_isomorphic(dom(apex(clim)), c)
+#pushout(f, p1)
+colimit(Span(f, p1))
+
+# DPO of slices: do the pushout complement and add an extra state
+R = GraphSlice(ACSetTransformation(Graph(2), two, V=[2, 1]))
+r = GraphSliceMorphism(A,R,ACSetTransformation(a, Graph(2), V=[1]))
+res = rewrite_match(f, r, g)
+@test is_isomorphic(dom(res), apex(coproduct(b, Graph(1))))
 end #module
