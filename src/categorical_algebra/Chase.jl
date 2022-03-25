1d1aebc06d268abb285c5dc6a86e6107e447d5a9
Kris <ksb@stanford.edu>
2022-03-25 12:28:27 -0400

    C-Rel/C-Set conversion, distinguish EGDs/TGDs, optionally chase in C-Rel

--- src/categorical_algebra/Chase.jl
+++ src/categorical_algebra/Chase.jl
@@ -1,16 +1,30 @@
 module Chase
-export ED, chase, chase_step, active_triggers
+export ED, chase, chase_crel, chase_step, to_c_rel, from_c_rel, crel_type,
+       egd, tgd
 
-using ...Theories
+using Reexport # needed to access struct_acset from CSetDataStructures
+
+using ...Theories, ...Present
 using ..CSets
 using ..FinSets
 using ..Limits
 import ...Theories: dom, codom
+@reexport using ...CSetDataStructures
+
+# EDs
+#####
 
 """
-A morphism S->T, encoding an embedded dependency. If the pattern S is matched
-(via a homomorphism S->I), we demand there exist a morphism T->I that makes the
-triangle commute.
+A morphism S->T, encoding an embedded dependency (or trigger). If the pattern S
+is matched (via a homomorphism S->I), we demand there exist a morphism T->I (for
+some database instance I) that makes the triangle commute in order to satisfy
+the dependency (if this is not the case, then the trigger is 'active').
+
+Homomorphisms can merge elements and introduce new ones. The former kind are
+called "equality generating dependencies" (EGDs) and the latter "tuple
+generating dependencies" (TGDs). Any homomorphism can be factored into EGD and
+TGD components by, respectively, restricting the codomain to the image or
+restricting the domain to the coimage.
 """
 struct ED
   ST :: ACSetTransformation
@@ -18,13 +32,135 @@ end
 
 dom(e::ED) = dom(e.ST)
 codom(e::ED) = codom(e.ST)
+image(f) = equalizer(legs(pushout(f,f))...)
+coimage(f) = coequalizer(legs(pullback(f,f))...)
+"""    egd(e::ED)
+Distill the component of a morphism that merges elements together
+"""
+egd(e::ED) = factorize(image(e.ST),e.ST)
+"""    tgd(e::ED)
+Distill the component of a morphism that adds new elements
+"""
+tgd(e::ED) = factorize(coimage(e.ST),e.ST)
+no_change(f) = is_isomorphic(dom(f), codom(f)) # id up to isomorphism
 
+"""Split a list of EDs into two lists of EGDs and TGDs"""
+function split_Σ(Σ::Vector{ED})
+  egds, tgds = [],[]
+  for ed in Σ
+    e, t = egd(ed), tgd(ed)
+    if !no_change(e)
+      push!(egds, e)
+    end
+    if !no_change(t)
+      push!(tgds, t)
+    end
+  end
+  egds => tgds
+end
+
+# C-Rel: note that (Span-C)-Set is our model for C-Rel
+######################################################
+
+# Convention for the names of span morphisms
+add_srctgt(m) = Symbol("src_$m") => Symbol("tgt_$m")
+
+"""    crel_type(x::StructACSet{S}) where S
+
+Convert an instance of a C-Set into an Span(C)-Set type.
+"""
+function crel_type(x::StructACSet{S}) where S
+  name_ = Symbol("rel_$(typeof(x).name.name)")
+  pres = Presentation(FreeSchema)
+  edges = vcat(add_srctgt.(hom(S))...)
+  xobs = Dict(map([ob(S)...,hom(S)...]) do s
+    s => add_generator!(pres, Ob(FreeSchema, s))
+  end)
+  for (h, hs, ht) in zip(hom(S), dom(S), codom(S))
+    s, t = add_srctgt(h)
+    add_generator!(pres, Hom(s, xobs[h], xobs[hs]))
+    add_generator!(pres, Hom(t, xobs[h], xobs[ht]))
+  end
+  expr = CSetDataStructures.struct_acset(name_, StructACSet, pres, index=edges)
+  eval(expr)
+  return eval(name_)
+end
+
+"""    to_c_rel(I::StructACSet{S}) where S
+A functor C-Set -> C-Rel, on objects. Can be applied safely to C-sets with
+undefined references.
+"""
+function to_c_rel(I::StructACSet{S}) where S
+  J = Base.invokelatest(crel_type(I))
+  for o in ob(S)
+    add_parts!(J, o, nparts(I, o))
+  end
+  for d in hom(S)
+    hs, ht = add_srctgt(d)
+    for (i, v) in filter(x->x[2] != 0, collect(enumerate(I[d])))
+      n = add_part!(J, d)
+      set_subpart!(J, n, hs, i)
+      set_subpart!(J, n, ht, v)
+    end
+  end
+  return J
+end
+
+"""   to_c_rel(f::ACSetTransformation)
+A functor C-Set -> C-Rel, on morphisms
+"""
+function to_c_rel(f::ACSetTransformation)
+  d, cd = to_c_rel.([dom(f), codom(f)])
+  init = NamedTuple([k=>collect(v) for (k,v) in pairs(components(f))])
+  homomorphism(d, cd; initial=init)
+end
+
+
+"""    from_c_rel(J::StructACSet,cset::StructACSet{S}) where S
+
+A functor C-Rel -> C-Set, on objects.
+
+This fails if a C-rel morphism is noninjective and returns a C-set with
+undefined references if the morphism isn't total (a return flag signals whether
+this occured).
+"""
+function from_c_rel(J::StructACSet,cset::StructACSet{S}) where S
+    res = typeof(cset)()
+    for o in ob(S)
+      add_parts!(res, o, nparts(J, o))
+    end
+    total = true
+    for (m, s) in zip(hom(S), dom(S))
+      msrc, mtgt = add_srctgt(m)
+      length(J[msrc]) == length(Set(J[msrc])) || error("noninjective $J")
+      total &= length(J[msrc]) != nparts(J, s)
+      for (domval, codomval) in zip(J[msrc], J[mtgt])
+        set_subpart!(res, domval, m, codomval)
+      end
+    end
+    return res => total
+end
+
+"""    from_c_rel(f::ACSetTransformation,cset::StructACSet{S}) where S
+
+A functor C-Rel -> C-Set, on morphisms.
+"""
+function from_c_rel(f::ACSetTransformation,cset::StructACSet{S}) where S
+  (d, dsucc), (cd, cdsucc) = [from_c_rel(x, cset) for x in [dom(f), codom(f)]]
+  comps = Dict([k=>v for (k,v) in pairs(components(f)) if k ∈ ob(S)])
+  ACSetTransformation(d, cd; comps...) => (dsucc && cdsucc)
+end
+
+# Chase
+#######
 
 VPSI = Vector{Pair{Symbol,Int}}
-"""
-Chase a CSet instance (attributes are tricky - TODO) given a list of embedded
-dependencies. This may not terminate, so a bound on the number of iterations is
-required.
+
+"""    chase(I::StructACSet, Σ::Vector{ED}, n::Int; verbose=false)
+
+Chase a C-Set or C-Rel instance (attributes are tricky - TODO) given a list of
+embedded dependencies. This may not terminate, so a bound `n` on the number of
+iterations is required.
 
     [,]
 Σ S  ⟶ Iₙ
@@ -35,24 +171,60 @@ There is a copy of S and T for each active trigger. A trigger is a map from S
 into the current instance. What makes it 'active' is that there is no morphism
 from T to I that makes the triangle commute.
 
-Each iteration constructs the above pushout square. The result is a morphism so
-that one can keep track of the provenance of elements in the original CSet instance within
-the chased result.
+Each iteration constructs the above pushout square. The result is a morphism, so
+that one can keep track of the provenance of elements in the original CSet
+instance within the chased result.
 
-Also eturns whether or not the result is due to success or timeout.
+If the initial instance and EDs are all C-sets, then the pushouts can take place
+in C-Set (which is more memory efficient). Otherwise, everything is converted to
+the Span(C) schema, which is sometimes necessary (for example, migrating data
+forward into a cyclic schema).
+
+Whether or not the result is due to success or timeout is returned as a boolean
+flag.
 """
 function chase(I::StructACSet, Σ::Vector{ED}, n::Int; verbose=false)
+
+  Σ_e_t = split_Σ(Σ)
   res = id(I)
+
   for i in 1:n
     if verbose println("Iter $i") end
-    next_morphism = chase_step(codom(res), Σ)
-    if isnothing(next_morphism)
-      return res => true # success
+    next_morphism = chase_step(codom(res), Σ_e_t)
+    if no_change(next_morphism)
+      return res => true
     else
       res = compose(res, next_morphism)
     end
   end
   return res => false # failure
+end
+
+"""    chase_crel(I::StructACSet, Σ::Vector{ED}, n::Int; I_is_crel::Bool=false, Σ_is_crel::Bool=false, cset_example::Union{StructACSet,Nothing}=nothing, verbose=false)
+
+`chase` works when both `I` and `Σ` are in C-Set or both are in C-Rel, though in the latter case the return value is a C-Rel morphism rather than one in C-Set.
+
+This function wraps `chase` and does the necessary conversions (computing in
+C-Rel if either the initial instance or EDs are provided in C-Rel), returning
+a result morphism in C-Set if the chase terminates.
+"""
+function chase_crel(I::StructACSet, Σ::Vector{ED}, n::Int;
+                    I_is_crel::Bool=false, Σ_is_crel::Bool=false,
+                    cset_example::Union{StructACSet,Nothing}=nothing, verbose=false)
+  # Process inputs
+  is_crel = I_is_crel || Σ_is_crel
+  !(is_crel && isnothing(cset_example)) || error("Need CSet for conversion")
+  I_rel = (is_crel && !I_is_crel) ? to_c_rel(I) : I
+  Σ_rel = (is_crel && !Σ_is_crel) ? to_c_rel.(Σ) : Σ
+
+  # Compute the chase
+  res, succ = chase(I_rel, Σ_rel, n; verbose=verbose)
+
+  # Postprocess results
+  if !succ
+    return res => false
+  end
+  return (is_crel ? from_c_rel(res, cset_example)[1] : res) => true
 end
 
 """
@@ -63,14 +235,13 @@ S->T->I = S->I).
 Optionally restrict to only considering a subset of the triggers with `ts`, a
 list of indices into the list of triggers.
 """
-function active_triggers(I::T_, Σ::Vector{ED}; ts::Vector{Int}=Int[]
-                  )::Vector{Pair{ACSetTransformation, ACSetTransformation}} where {
+function active_triggers(I::T_, Σ; init::Union{NamedTuple, Nothing}
+          )::Vector{Pair{ACSetTransformation, ACSetTransformation}} where {
                     T_<:StructACSet}
   maps = Pair{ACSetTransformation, ACSetTransformation}[]
-  for ed_ in Σ
-    ed = ed_.ST
-    for trigger in last.(filter(ih->isempty(ts) || ih[1] in ts,
-                                collect(enumerate(homomorphisms(dom(ed), I)))))
+  for ed in Σ
+    kw = Dict(isnothing(init) ? [] : [:initial=>init])
+    for trigger in homomorphisms(dom(ed), I; kw...)
       if isnothing(extend_morphism(trigger, ed))
         push!(maps, trigger => ed)
       end
@@ -84,16 +255,28 @@ Run a single chase step. Optionally select a subset of triggers to fire on.
 This selection could be generalized to be a function which does a computation
 to determine which firings are useful.
 """
-function chase_step(I::StructACSet, Σ::Vector{ED}; ts::Vector{Int}=Int[])
-    ats = active_triggers(I, Σ; ts=ts)
-    if isempty(ats)  # Early termination condition
-      return I, ins, outs
+function chase_step(I::StructACSet, Σ; init::Union{NamedTuple, Nothing}=nothing)
+    Σe, Σt = Σ
+    ats = active_triggers(I, Σt; init=init)
+    res = isempty(ats) ? id(I) : fire_triggers(ats) # first: fire TGDs
+    while true
+      ats = active_triggers(codom(res), Σe; init=init)
+      res_ = isempty(ats) ? id(codom(res)) : fire_triggers(ats)
+      if force(res_) == force(id(codom(res)))
+        return res
+      else
+        res = compose(res, res_)
+      end
     end
-    r_i_maps, r_s_maps = first.(ats), last.(ats)
-    # Combine each list of morphisms into a single one & take pushout
-    I_po = pushout(copair(r_i_maps), oplus(r_s_maps))
-    # Propagate info about which vertices are inputs/outputs & update I
-    return legs(I_po)[1]
+    en
+end
+
+function fire_triggers(ats)
+  r_i_maps, r_s_maps = first.(ats), last.(ats)
+  # Combine each list of morphisms into a single one & take pushout
+  I_po = pushout(copair(r_i_maps), oplus(r_s_maps))
+  # Propagate info about which vertices are inputs/outputs & update I
+  return legs(I_po)[1]
 end
 
 
@@ -125,6 +308,5 @@ function extend_morphism(f::ACSetTransformation, g::ACSetTransformation;
   end
   homomorphism(codom(g), codom(f); initial=NamedTuple(init), monic=monic)
 end
-
 
 end # module
--- test/categorical_algebra/Chase.jl
+++ test/categorical_algebra/Chase.jl
@@ -6,8 +6,8 @@ using Catlab.Present
 using Catlab.Theories
 using Catlab.CategoricalAlgebra
 
-# School example from Fast Left Kan Extensions paper
-#---------------------------------------------------
+# School example
+#---------------
 @present ThSchool(FreeSchema) begin
   (TA, Student, Faculty, Person)::Ob
   t_s::Hom(TA, Student)
@@ -21,50 +21,26 @@ end
 
 # Construct ED to enforce the path equality
 ed1 = @acset School begin
-    TA = 1
-    Student = 1
-    Faculty = 1
-    Person = 2
-    t_s = [1]
-    t_f = [1]
-    s_p = [1]
-    f_p = [2]
+    TA = 1; Student = 1; Faculty = 1; Person = 2
+    t_s = [1]; t_f = [1]; s_p = [1]; f_p = [2]
 end
 ed2 = @acset School begin
-    TA = 1
-    Student = 1
-    Faculty = 1
-    Person = 1
-    t_s = [1]
-    t_f = [1]
-    s_p = [1]
-    f_p = [1]
+  TA = 1 ;Student = 1 ;Faculty = 1 ;Person = 1
+  t_s = [1] ;t_f = [1] ;s_p = [1] ;f_p = [1]
 end
 d = Dict([:TA=>[1], :Student=>[1], :Faculty=>[1], :Person=>[1,1]])
 ed = ED(ACSetTransformation(ed1, ed2; d...))
 # initializing db from an instance with 5 faculty, 4 students, and 2 TAs
 # when we freely add elements due to functionality, we get 9 elements in Person
 unchased = @acset School begin
-    TA = 2
-    Student = 4
-    Faculty = 5
-    Person = 9
-    t_s = [1,2]
-    t_f = [1,2]
-    s_p = [1,2,3,4]
-    f_p = [5,6,7,8,9]
+    TA = 2; Student = 4; Faculty = 5; Person = 9
+    t_s = [1,2]; t_f = [1,2]; s_p = [1,2,3,4]; f_p = [5,6,7,8,9]
 end
 # Because the first two Faculty are the same people as the first two students
 # We expect the result to have only 7 people and for f_p[1:2] == s_p[1:2]
 expected = @acset School begin
-    TA = 2
-    Student = 4
-    Faculty = 5
-    Person = 7
-    t_s = [1,2]
-    t_f = [1,2]
-    s_p = [1,2,3,4]
-    f_p = [1,2,5,6,7]
+    TA = 2; Student = 4; Faculty = 5; Person = 7
+    t_s = [1,2]; t_f = [1,2]; s_p = [1,2,3,4]; f_p = [1,2,5,6,7]
 end
 
 @test is_isomorphic(expected, codom(first(chase(unchased, [ed], 1))))
@@ -79,6 +55,23 @@ biarr = deepcopy(pg)       # 1 <-> 2
 add_edge!(biarr, 2, 1)
 ed = ED(ACSetTransformation(pg, biarr, V=[2,1], E=[2]))
 
+# We can factor a ED that combines EGD and TGD aspects
+etgd_s = path_graph(Graph, 3)
+etgd_t = @acset Graph begin
+  V = 2; E=3; src=[1,1,2]; tgt=[1,2,1]
+end
+# cset common to both tgd and egd
+core = @acset Graph begin V=2; E=2; src=[1,2]; tgt=[2,1] end
+
+# This adds a self loop to #1 and merges #1/#3
+etgd = ED(ACSetTransformation(etgd_s,etgd_t; V=[1,2,1], E=[2,3]))
+
+@test is_isomorphic(codom(egd(etgd)), core) # EGD has no extra self-edge
+@test collect(egd(etgd)[:V]) == [1,2,1] # but it does merge vertices
+
+@test is_isomorphic(dom(tgd(etgd)), core) # TGD does not merge
+@test codom(tgd(etgd)) == codom(etgd.ST) # but it does add the self edge
+
 # Initial instance
 tri = path_graph(Graph, 3)
 add_edge!(tri, 3, 1)
@@ -90,5 +83,34 @@ add_edges!(sym_tri, [2,3,1],[1,2,3])
 # Tests
 @test is_isomorphic(sym_tri, codom(first(chase(tri, [ed], 3)))) # terminates in one step
 @test biarr == codom(first(chase(biarr, [ed], 3)))  # terminates instantly
+
+# C-Rel
+@present ThLoop(FreeSchema) begin
+  X::Ob
+  x::Hom(X, X)
+  compose(x, x, x) == compose(x, x)
+end
+@acset_type Loop(ThLoop)
+LoopRel = crel_type(Loop())
+
+# Constraints to encode that x is a function
+injective_l = @acset LoopRel begin  X=3; x=2; src_x=[1,1]; tgt_x=[2,3] end
+injective_r = @acset LoopRel begin  X=2; x=1; src_x=[1]; tgt_x=[2] end
+ED_injective = homomorphism(injective_l, injective_r)
+total_l = @acset LoopRel begin X=1 end
+ED_total = homomorphism(total_l, injective_r)
+# x-path of length 3 = x-path of length 2
+three_two_l = @acset LoopRel begin
+  X=6; x=5; src_x=[1,2,3,1,5]; tgt_x=[2,3,4,5,6] end
+three_two_r = @acset LoopRel begin
+  X=5; x=5; src_x=[1,2,3,1,5]; tgt_x=[2,3,4,5,4] end
+ED_three_two = ACSetTransformation(three_two_l, three_two_r;
+                                   X=[1,2,3,4,5,4], x=[1,2,3,4,5])
+ΣX = ED.([ED_injective,ED_total,ED_three_two])
+# Compute the chase
+res, succ = chase_crel(total_l, ΣX, 5; I_is_crel=true, Σ_is_crel=true,
+                      cset_example=Loop(), verbose=false)
+@test succ
+@test codom(res) == @acset Loop begin X=3; x=[2,3,3] end
 
 end # module
