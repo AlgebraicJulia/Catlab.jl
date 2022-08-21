60b1c121cc0568acf0112e91296f773b444bc7a2
Samuel Steakley <sam.steakley@gmail.com>
2022-08-21 14:00:15 -0500

    Create syntax system: FreeGroupoid

--- src/theories/Groupoid.jl
+++ src/theories/Groupoid.jl
@@ -1,4 +1,4 @@
-export ThGroupoid, inverse
+export ThGroupoid, FreeGroupoid, inverse
 
 # Groupoid
 ##########
@@ -11,4 +11,11 @@ export ThGroupoid, inverse
 
   f ⋅ inverse(f) == id(A) ⊣ (A::Ob, B::Ob, f::(A → B))
   inverse(f) ⋅ f == id(B) ⊣ (A::Ob, B::Ob, f::(A → B))
+end
+
+@syntax FreeGroupoid{ObExpr,HomExpr} ThGroupoid begin
+  compose(f::Hom, g::Hom) = associate_id_inverse(new(f,g; strict=true), id,
+                                                 inverse)
+  inverse(f::Hom) = distribute_unary(involute(new(f)), inverse, compose,
+                                     unit=id, contravariant=true)
 end
