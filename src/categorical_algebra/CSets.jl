02fac6820a38febcf8c70307b64f3ff18b6f3789
Kris Brown <kris@topos.institute>
2022-11-30 13:33:22 -0800

    documentation

--- src/categorical_algebra/CSets.jl
+++ src/categorical_algebra/CSets.jl
@@ -828,6 +828,21 @@ function limit(::Type{<:Tuple{ACS,TightACSetTransformation}}, diagram) where
   pack_limit(ACS, diagram, Xs, limits)
 end
 
+"""
+A limit of a diagram of ACSets with LooseACSetTransformations.
+
+For general diagram shapes, the apex of the categorical limit will not have
+clean Julia types for its attributes, i.e. predicates will be needed to further 
+constrain them. `product_attrs` must be turned on in order to avoid an error in 
+cases where predicates would be needed. 
+
+`product_attrs=true` will take a limit of the purely combinatorial data, and 
+the attribute data of the apex is dictated purely by the ACSets that are have 
+explicit cone legs: the value of an attribute (e.g. `f`) for the i'th part in  
+the apex is the product `(f(π₁(i)), ..., f(πₙ(i)))` where π maps come from 
+the combinatorial part of the limit legs. The type components of the j'th leg of 
+the limit is just the j'th cartesian projection.
+"""
 function limit(::Type{Tuple{ACS,Hom}}, diagram; product_attrs::Bool=false) where
     {S, ACS <: StructACSet{S}, Hom <: LooseACSetTransformation}
   limits = map(limit, unpack_diagram(diagram, all=!product_attrs))
@@ -841,7 +856,7 @@ function limit(::Type{Tuple{ACS,Hom}}, diagram; product_attrs::Bool=false) where
     ACSUnionAll = Base.typename(ACS).wrapper
     ACSUnionAll{(eltype(ob(attr_lims[d])) for d in attrtypes(S))...}
   end
-  
+
   type_components = [
     Dict(d=>legs(attr_lims[d])[i] for d in attrtypes(S)) for i in eachindex(Xs)]
   
