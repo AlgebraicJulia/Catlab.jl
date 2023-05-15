802f2bfab5c44876ff4c770fcc1040a4ddd52657
Micah Halter <micah@mehalter.com>
2023-05-15 10:24:30 -0400

    FIX: actually working convex/scs package extension

---
+++ ext/CatlabConvexExt.jl
@@ -0,0 +1,31 @@
+module CatlabConvexExt
+
+using Convex
+
+import Catlab.Graphics.WiringDiagramLayouts: has_port_layout_method, solve_isotonic
+
+has_port_layout_method(::Type{Val{:isotonic}}) = true
+
+function solve_isotonic(y::Vector, solver=nothing;
+                        loss=sumsquares, lower::Number=-Inf, upper::Number=Inf,
+                        pad::Number=0)
+  if isnothing(solver)
+    error("Isotonic solver backend not available")
+  end
+  if isempty(y)
+    return empty(y)
+  end
+  x = Variable(length(y))
+  constraints = Constraint[ x[i] - x[i-1] >= pad for i in 2:length(x) ]
+  if lower != -Inf
+    push!(constraints, x[1] >= lower)
+  end
+  if upper != Inf
+    push!(constraints, x[end] <= upper)
+  end
+  solve!(minimize(loss(x-y), constraints), solver;
+         verbose=false, silent_solver=true)
+  length(x) == 1 ? [ x.value ] : dropdims(x.value, dims=2) # XXX: MATLAB-ism.
+end
+
+end
--- ext/CatlabSCSExt.jl
+++ ext/CatlabSCSExt.jl
@@ -2,39 +2,8 @@ module CatlabSCSExt
 
 using SCS
 
-# HACK: sort of a hacky way around the lack of multiple package dependencies for an extension
-# Hopefully this is a feature that can be added and this hack can be removed
-using Pkg
-const HasConvex = haskey(Pkg.project().dependencies, "Convex")
-if HasConvex
-  using Convex
-end
+import Catlab.Graphics.WiringDiagramLayouts: solve_isotonic
 
-import Catlab.Graphics.WiringDiagramLayouts: has_port_layout_method, solve_isotonic
-
-has_port_layout_method(::Type{Val{:isotonic}}) = HasConvex
-
-function solve_isotonic(y::Vector; solver=SCS.Optimizer,
-                        loss=sumsquares, lower::Number=-Inf, upper::Number=Inf,
-                        pad::Number=0)
-  if !HasConvex
-    error("Convex is required along with SCS for isotonic regression.")
-  end
-
-  if isempty(y)
-    return empty(y)
-  end
-  x = Variable(length(y))
-  constraints = Constraint[ x[i] - x[i-1] >= pad for i in 2:length(x) ]
-  if lower != -Inf
-    push!(constraints, x[1] >= lower)
-  end
-  if upper != Inf
-    push!(constraints, x[end] <= upper)
-  end
-  solve!(minimize(loss(x-y), constraints), solver;
-         verbose=false, silent_solver=true)
-  length(x) == 1 ? [ x.value ] : dropdims(x.value, dims=2) # XXX: MATLAB-ism.
-end
+solve_isotonic(y::Vector, solver::Nothing; kw...) = solve_isotonic(y, SCS.Optimizer; kw...)
 
 end
