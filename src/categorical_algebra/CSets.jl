07f5ee1ef059690eda861433fb2175e856a0500a
Evan Patterson <evan@epatters.org>
2023-05-17 16:50:09 -0700

    CLEANUP: Use meaningful variable names in `parse_json_acset`.

--- src/categorical_algebra/CSets.jl
+++ src/categorical_algebra/CSets.jl
@@ -1459,24 +1459,31 @@ parse_json_acset(d::DynamicACSet, input::AbstractDict) =
 
 function _parse_json_acset(cons, input::AbstractDict)
   out = cons()
-  for (k,v) ∈ input
-    add_parts!(out, Symbol(k), length(v))
+  for (type, rows) ∈ input
+    add_parts!(out, Symbol(type), length(rows))
   end
-  for l ∈ values(input)
-    for (i, j) ∈ enumerate(l)
-      for (k,v) ∈ j
-        k == "_id" && continue
-        is_attr = Symbol(k) ∈ attrs(acset_schema(out); just_names=true)
-        vtype = is_attr ? attr_type(out, Symbol(k)) : Int
-        v = v isa Dict && haskey(v, "val") ? AttrVar(v["val"]) : vtype(v)
-        set_subpart!(out, i, Symbol(k), v)
+  for rows ∈ values(input)
+    for (rownum, row) ∈ enumerate(rows)
+      for (k, v) ∈ row
+        k = Symbol(k)
+        if k == :_id
+          # For now, IDs are assumed to coincide with row number.
+          @assert rownum == v
+          continue
+        end
+        is_attr = k ∈ attrs(acset_schema(out); just_names=true)
+        vtype = is_attr ? attr_type(out, k) : Int
+        v = v isa AbstractDict && haskey(v, "val") ?
+          AttrVar(v["val"]) : vtype(v)
+        set_subpart!(out, rownum, k, v)
       end
     end
   end
   out
 end
-function parse_json_acset(::Type{T}, input::AbstractString) where T <: ACSet
-  parse_json_acset(T, JSON.parse(input))
+
+function parse_json_acset(target, input::AbstractString)
+  parse_json_acset(target, JSON.parse(input))
 end
 
 """ Deserialize an ACSet object from a JSON file.
