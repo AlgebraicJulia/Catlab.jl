2e4fae89839ccc73d9c5f3808b3d3dfd1caee220
Kris Brown <kris@topos.institute>
2024-01-17 11:29:57 -0800

    add benchmark file

---
+++ benchmark/HomSearch.jl
@@ -0,0 +1,55 @@
+# Benchmark various hom search algorithms against each other.
+# In the future, benchmark against external solvers, too.
+
+using BenchmarkTools
+const SUITE = BenchmarkGroup()
+
+using Random
+using Catlab
+using Catlab.CategoricalAlgebra.HomSearch: compile_search
+
+Random.seed!(1)
+
+bench = SUITE["HomSearch"] = BenchmarkGroup()
+btsbench = bench["Backtracking"] = BenchmarkGroup()
+vmnbench = bench["VM_neighbor"] = BenchmarkGroup()
+vmcbench = bench["VM_connected"] = BenchmarkGroup()
+
+# DDS
+#####
+
+@present SchDDS(FreeSchema) begin
+  X::Ob
+  Φ::Hom(X,X)
+end
+
+@acset_type DDS(SchDDS, index=[:Φ])
+
+DDS(i::Int) = @acset DDS begin X=i; Φ=[rand(1:i) for _ in 1:i] end # random DDS
+
+small_dds = [DDS(5) for _ in 1:2]
+big_dds = [DDS(200) for _ in 1:2]
+
+prog1 = compile_search.(small_dds, strat=:neighbor);
+prog2 = compile_search.(small_dds, strat=:connected);
+
+for (i, x) in enumerate(small_dds), (j, y) in enumerate(big_dds)
+  btsbench["DDS-$i-$j"] = @benchmarkable homomorphisms($x, $y)
+  vmnbench["DDS-$i-$j"] = @benchmarkable homomorphisms($x, $y; alg=VMSearch(), prog=$(prog1[i]))
+  vmcbench["DDS-$i-$j"] = @benchmarkable homomorphisms($x, $y; alg=VMSearch(), prog=$(prog2[i]))
+end
+
+# Graph
+#######
+
+c3 = cycle_graph(Graph, 3)
+
+prog1 = compile_search(c3, strat=:neighbor);
+prog2 = compile_search(c3, strat=:connected);
+
+for (i, G) in [(i, erdos_renyi(Graph, 150, 0.05)) for i in 1:5]
+  btsbench["graph-$i"] = @benchmarkable homomorphisms($c3, $G)
+  vmnbench["graph-$i"] = @benchmarkable homomorphisms($c3, $G; alg=VMSearch(), prog=$prog1)
+  vmcbench["graph-$i"] = @benchmarkable homomorphisms($c3, $G; alg=VMSearch(), prog=$prog2)
+end
+
--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,6 +2,10 @@ using BenchmarkTools
 
 const SUITE = BenchmarkGroup()
 
+module BenchmarkHomSearch
+  include("HomSearch.jl")
+end
+
 module BenchmarkFinSets
   include("FinSets.jl")
 end
@@ -10,5 +14,6 @@ module BenchmarkGraphs
   include("Graphs.jl")
 end
 
+SUITE["HomSearch"] = BenchmarkHomSearch.SUITE
 SUITE["FinSets"] = BenchmarkFinSets.SUITE
 SUITE["Graphs"] = BenchmarkGraphs.SUITE
