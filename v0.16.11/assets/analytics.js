55e70afebdc9a84fc229f004632dfa721bc031bf
Documenter.jl <documenter@juliadocs.github.io>
2024-06-21 18:58:27 +0000

    build based on ee6a034

---
+++ v0.16.11/assets/analytics.js
@@ -0,0 +1,11 @@
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="https://matomo.mehalter.com/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '8']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
