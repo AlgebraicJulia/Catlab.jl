7e4482a42c181697641ac08194737cc19f31f329
quffaro <cuffaro.matt@gmail.com>
2024-06-30 08:50:21 -0400

    Update jobscript.sh

--- .buildkite/jobscript.sh
+++ .buildkite/jobscript.sh
@@ -6,4 +6,4 @@ echo "Running Tests..."
 julia --project -t 16 -e 'using Pkg; Pkg.status(); Pkg.test()'
 
 echo "Building Documentation..."
-julia --project=docs -t 16 -e'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.status(); Pkg.instantiate(); include("docs/make.jl")'
+julia --project=docs -t 16 -e 'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.status(); Pkg.instantiate(); include("docs/make.jl")'
