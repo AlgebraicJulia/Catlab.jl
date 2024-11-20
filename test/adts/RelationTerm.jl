471ecd44db734ceb94ae19067612030016137a9e
Christian Scaff <cscaff@ufl.edu>
2024-11-20 00:14:53 -0500

    Added Typed UWD Test

--- test/adts/RelationTerm.jl
+++ test/adts/RelationTerm.jl
@@ -11,18 +11,11 @@ using Catlab.WiringDiagrams.UndirectedWiringDiagrams
 using Catlab.Programs.RelationalPrograms
 
 
-#@testset "UWD Construction" begin
+@testset "UWD Construction" begin
   # parsed = @relation (x,z) where (x,y,z) begin
   # R(x,y)
   # S(y,z)
   # end
-
-  # TODO: Update Show function in RelationTerm to work with new outer ports
-  # TODO: need to bring the implementation of construct into compliance with existing catlab impl.
-  # TODO: add outer ports to UWDExpr and then to the construct function
-  # TODO: add outer ports to parser
-  # TODO; relation macro should emit a RelationTerm instead of using the PEG Parser.
-  # TODO: refactor the lexer out of the parser and make those reusable.
 
   v1 = Untyped(:x)
   v2 = Untyped(:y)
@@ -44,15 +37,39 @@ using Catlab.Programs.RelationalPrograms
 
   #Test Equality
   @test uwd_result == d
-  d
 
-  
-  #end
+  # parsed = @relation (x,y,z) where (x::X, y::Y, z::Z, w::W) begin
+  # R(x,w)
+  # S(y,w)
+  # T(z,w)
+  # end
 
-# DEBUG
+  v1 = Typed(:x, :X)
+  v2 = Typed(:y, :Y)
+  v3 = Typed(:z, :Z)
+  v4 = Typed(:w, :W)
+  c = [v1, v2, v3, v4]
+  op = [v1, v2, v3]
+  s = [Statement(:R, [v1, v4]), Statement(:S, [v2, v4]), Statement(:T, [v3, v4])]
+  u = UWDExpr(op, c, s)
 
+  uwd_result = RelationTerm.construct(RelationDiagram, u)
 
-
-
+  d = RelationDiagram([:X,:Y,:Z])
+  add_box!(d, [:X,:W], name=:R)
+  add_box!(d, [:Y,:W], name=:S)
+  add_box!(d, [:Z,:W], name=:T)
+  add_junctions!(d, [:X,:Y,:Z,:W], variable=[:x,:y,:z,:w])
+  set_junction!(d, [1,4,2,4,3,4])
+  set_junction!(d, [1,2,3], outer=true)
+  @test uwd_result == d
+end
 
 end
+
+# TODO: Update Show function in RelationTerm to work with new outer ports
+# TODO: need to bring the implementation of construct into compliance with existing catlab impl.
+# TODO: add outer ports to UWDExpr and then to the construct function
+# TODO: add outer ports to parser
+# TODO; relation macro should emit a RelationTerm instead of using the PEG Parser.
+# TODO: refactor the lexer out of the parser and make those reusable.
