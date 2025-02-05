3fd1e0e20ce0bbc8992ffa816dfd4cda22a5c506
Christian Scaff <97995580+cscaff@users.noreply.github.com>
2025-02-05 10:48:33 -0500

    Removed Space in ADTSCore module

--- src/adts/ADTsCore.jl
+++ src/adts/ADTsCore.jl
@@ -2,7 +2,6 @@
 """
 module ADTsCore
 
-
 """    AbstractTerm
 
 The super type for all ADT types. This abstract type exists so that we can write generic methods that work on any term in any of the domain specific syntaxes.
